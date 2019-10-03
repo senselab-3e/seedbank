21b6ccec697a5eaa3c54d1686a8aa5fdcd0d4b11
Joel Auerbach <joelauerbach1@gmail.com>
2019-10-03 12:16:12 -0400

    Add build

---
+++ client/build/precache-manifest.280a722389c130a656c3d3aaf02b3c80.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1f79ad5422d37fd2dabf523f475ae9c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc43474a0a62fabdaf66",
+    "url": "/static/js/2.5de23a34.chunk.js"
+  },
+  {
+    "revision": "3f33d3b81f8be90d0f92",
+    "url": "/static/js/main.eb46e890.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
