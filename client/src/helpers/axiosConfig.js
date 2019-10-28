97d1215111082eb3c293832592dd806f64174aec
Joel Auerbach <joelauerbach1@gmail.com>
2019-10-28 00:53:31 -0400

    IFFE test

--- client/src/helpers/axiosConfig.js
+++ client/src/helpers/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const app = axios.create({
   // baseURL: process.env.NODE_ENV === 'prod' ? 'wh464179.ispot.cc' : 'http://localhost:3000'
-  baseURL: 'https://3ecologies-seedbank.com'
+  baseURL: 'https://3ecologies-seedbank.com/api/'
 });
 
 // axios consumes rejected API responses by default,
--- client/src/pages/auth.js
+++ client/src/pages/auth.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import AuthLogin from '../components/AuthLogin';
 import AuthSignup from '../components/AuthSignup';
 import AuthLogout from '../components/AuthLogout';
+import "../components/AuthTest";
 
 const AuthPage = () => {
 	return (
