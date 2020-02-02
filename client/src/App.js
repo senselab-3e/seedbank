f32217438ebbb4face629d947c6e09cdb1222ab6
cepholopoddreamz <leslie.plumb@gmail.com>
2020-02-02 07:59:19 +0000

    added Router and more pathways About Component test and checking if auth components are still ok tomorrow ill run knex

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,79 @@
-import React, { Component } from 'react';
-import { Route, Switch, Link } from 'react-router-dom';
-import withAuth from './helpers/withAuth';
-import EventsPage from './pages/events';
-import AuthPage from './pages/auth';
+import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link
+  //   useRouteMatch,
+  //   useParams
+} from "react-router-dom";
+import withAuth from "./helpers/withAuth";
+import EventsPage from "./pages/events";
+import AuthPage from "./pages/auth";
+import About from "./pages/About";
 
 const MainPage = () => {
-	return (
-		<div>
-			<p>o0Oo0o 3ntryw4y o0oO0o</p>
-			<br/>
-			<Link to='/events'>Go to events</Link>
-			<br/><br/>
-			<Link to='/auth'>Login or signup</Link>
-		</div>
-	);
+  return (
+    <div>
+      <p>o0Oo0o 3ntryw4y o0oO0o</p>
+      <br />
+      <Link to="/events">Go to events</Link>
+      <br />
+      <br />
+      <Link to="/auth">Login or signup</Link>
+      <Link to="/about3e">3E About</Link>
+      <Link to="/oOoOs">404 oOoO Portal</Link>
+      <Link to="/patches">Patches</Link>
+    </div>
+  );
+};
+
+function oOoO() {
+  return <h2>oOoO</h2>;
+}
+
+function Patches() {
+  return <h2>Patches</h2>;
 }
 
 class App extends Component {
   render() {
     return (
-    	<Switch>
-    		<Route exact path='/auth' component={AuthPage} />
-    		<Route exact path='/events' component={withAuth(EventsPage)} />
-    		<Route exact path='/' component={MainPage} />
-    	</Switch>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/entryway">EntryWay</Link>
+              </li>
+              <li>
+                <Link to="/about3e">3E About</Link>
+              </li>
+              <li>
+                <Link to="/oOoOs">404 oOoO Portal</Link>
+              </li>
+              <li>
+                <Link to="/patches">Patches</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <Switch>
+          <Route path="/about3e">
+            <About />
+          </Route>
+          <Route path="/oOoOs">
+            <oOoO />
+          </Route>
+          <Route path="/patches">
+            <Patches />
+          </Route>
+          <Route exact path="/auth" component={AuthPage} />
+
+          <Route exact path="/events" component={withAuth(EventsPage)} />
+          <Route exact path="/" component={MainPage} />
+        </Switch>
+      </Router>
     );
   }
 }
---
+++ client/src/pages/About.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+// import ReactDOM from "react-dom";
+
+class About extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return <div> ABOUT 3 Ecologgiiiesssss </div>;
+  }
+}
+
+export default About;
