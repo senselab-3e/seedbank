2720fc755612e48218aa604e21871ff2aedbdf50
cepholopoddreamz <leslie.plumb@gmail.com>
2020-02-04 23:09:29 +0000

    potentially i think found the bug that was keeping our pages from refreshing at a 404 error on subpathways after sanity checking everything in server js and then all the reactrouterdom scripts it seems to be the ./ that was inside the homepage of the json file which i then changed to . for current file

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,7 @@ import React, { Component } from "react";
 import {
   BrowserRouter as Router,
   Route,
-  Switch,
-  Link
-  //   useRouteMatch,
-  //   useParams
+  Switch
 } from "react-router-dom";
 import withAuth from "./helpers/withAuth";
 import EventsPage from "./pages/events";
@@ -14,73 +11,33 @@ import About from "./pages/About";
 import Header from "./partials/Header";
 import Glitch from "./pages/Glitch";
 
-const MainPage = () => {
+
+function Home() {
   return (
     <div>
-      <p>o0Oo0o 3ntryw4y o0oO0o</p>
-      <br />
-      <Link to="/events">Go to events</Link>
-      <Link to="/auth">Login or signup</Link>
+      <h2>Home Hello</h2>
     </div>
   );
-};
+}
 
-// const OoOoO = () => {
-//   return (
-//     <div>
-//       <h2>glitch </h2>
-//     </div>
-//   );
-// };
-
-const Patches = () => {
-  return <h3>Patches</h3>;
-};
 
 class App extends Component {
   render() {
     return (
-      <div>
+      
+        <Router> 
         <Header />
-        <Router>
-          <div>
-            <nav>
-              <ul>
-                <li>
-                  <Link to="/">Home Login</Link>
-                </li>
-                <li>
-                  <Link to="/entryway">EntryWay</Link>
-                </li>
-                <li>
-                  <Link to="/about3e">3E About</Link>
-                </li>
-                <li>
-                  <Link to="/oOoOs">404 oOoO Portal</Link>
-                </li>
-                <li>
-                  <Link to="/patches">Patches</Link>
-                </li>
-              </ul>
-            </nav>
-          </div>
-          <Switch>
-            <Route path="/about3e">
-              <About />
-            </Route>
-            <Route path="/oOoOs">
-              <Glitch />
-            </Route>
-            <Route path="/patches">
-              <Patches />
-            </Route>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route path="/about3e" component={About}/>
+            <Route path="/oOoOs" component={Glitch}/>
+            <Route path="/patches" render={() => <div>Patches</div>}/>
+            <Route path="/entryway"render={() => <div>Entry</div>}/>
             <Route exact path="/auth" component={AuthPage} />
-
             <Route exact path="/events" component={withAuth(EventsPage)} />
-            <Route exact path="/" component={MainPage} />
-          </Switch>
+            </Switch>
         </Router>
-      </div>
+      
     );
   }
 }
