16fae2bf837bba2c5a4cb9774700de24925f3fa1
LesliePlumb <lplumb@fa-ev10785sens2.local>
2020-02-04 23:03:51 -0500

    removed the wrong file (withAuth, rather then checkAuth) so i added it back

--- client/src/helpers/withAuth.js
+++ client/src/helpers/withAuth.js
@@ -1,26 +1,29 @@
-import React, { Component } from "react";
-import { Redirect } from "react-router-dom";
-import axios from "axios";
+import React, {
+  Component
+} from 'react';
+import {
+  Redirect
+} from 'react-router-dom';
+import axios from 'axios';
 // import app from "./axiosConfig";
 
 // wraps component in router config to require authentication
 
-export default function checkAuth(ComponentToProtect) {
+export default function withAuth(ComponentToProtect) {
   return class extends Component {
     constructor() {
       super();
       this.state = {
         loading: true,
-        redirect: false
+        redirect: false,
       };
     }
     componentDidMount() {
       const headers = {
-        authorization: "Bearer " + localStorage.getItem("token")
+        'authorization': 'Bearer ' + localStorage.getItem('token')
       };
 
-      axios
-        .get("/api/auth/verify", {
+      axios.get('/api/auth/verify', {
           headers: headers
         })
         .then(res => {
@@ -43,18 +46,25 @@ export default function checkAuth(ComponentToProtect) {
         });
     }
     render() {
-      const { loading, redirect } = this.state;
+      const {
+        loading,
+        redirect
+      } = this.state;
       if (loading) {
         return null;
       }
       if (redirect) {
-        return <Redirect to="/auth" />;
+        return <Redirect to = "/auth" / > ;
       }
-      return (
-        <React.Fragment>
-          <ComponentToProtect {...this.props} />{" "}
-        </React.Fragment>
+      return ( <
+        React.Fragment >
+        <
+        ComponentToProtect {
+          ...this.props
+        }
+        /> <
+        /React.Fragment>
       );
     }
-  };
+  }
 }
