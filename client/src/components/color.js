e95c107338318c3ca43ea4554b11d54e4998748a
LesliePlumb <lplumb@fa-ev10785sens2.local>
2020-02-04 23:21:42 -0500

    studying different approaches to dynamic styling in components to compare with dynamic css methods

--- client/src/components/color.js
+++ client/src/components/color.js
@@ -1,29 +1,27 @@
 const Color = {
-    blue : '#70CFE1',
-    green : '#85C85D',
-    yellow : "#E5CF41",
-    random : "#81479b"
+  blue: "#70CFE1",
+  green: "#85C85D",
+  yellow: "#E5CF41",
+  random: "#81479b"
+};
+
+/* eslint-disable import/first */
+const divStyle = {
+  color: Color.blue,
+  background: Color.yellow,
+  height: "300px"
+};
+
+import React, { Component } from "react";
+
+export class color extends Component {
+    
+  render() {
+    getInitialState: function () {
+        return { color: Color.green };
+        },
+    return <div style={divStyle}>Hello World!</div>;
   }
+}
 
-  
-  
-  import React, { Component } from 'react'
-  
-  export class color extends Component {
-
-    const divStyle = {
-        color: Color.blue,
-        background: Color.yellow,
-        height: '300px'
-    };
-
-    render() {
-        return (
-            <div style={divStyle}>Hello World!
-            </div>
-        )
-    }
-  }
-  
-  export default color
-  
+export default color;
