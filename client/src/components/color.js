e7391b878a72a5a79ed95d93ea2ad6e7462aa7ff
LesliePlumb <lplumb@fa-ev10785sens2.local>
2020-02-04 22:49:56 -0500

    building color component for react styling

--- client/src/components/color.js
+++ client/src/components/color.js
@@ -1,5 +1,21 @@
-import React from "react";
-
-export default function color() {
-  return <div></div>;
-}
+const Color = {
+    blue : '#70CFE1'
+    green : '#85C85D',
+    yellow : "#E5CF41",
+    random ="#81479b"
+  }
+  
+  import React, { Component } from 'react'
+  
+  export class color extends Component {
+      render() {
+          return (
+              <div>
+                  
+              </div>
+          )
+      }
+  }
+  
+  export default color
+  
