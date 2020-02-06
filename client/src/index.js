b18b7b257361e3ddaea6765af05fd58ffa55e4c7
cepholopoddreamz <leslie.plumb@gmail.com>
2020-02-06 00:32:05 +0000

    attempting another hot reload work around in index

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import App from './App';
-import ReactDOM from 'react-dom';
+import React from "react";
+import App from "./App";
+import ReactDOM from "react-dom";
 // import { BrowserRouter as Router } from 'react-router-dom';
 // import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootEl = document.getElementById("root");
+
+ReactDOM.render(<App />, rootEl);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 // serviceWorker.register();
+if (module.hot) {
+  module.hot.accept("./App", () => {
+    const NextApp = require("./App").default;
+    ReactDOM.render(<NextApp />, rootEl);
+  });
+}
