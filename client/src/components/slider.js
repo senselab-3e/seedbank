13bd68264e4a208bea74407eb2d88c818b93f15d
LesliePlumb <lplumb@fa-ev10785sens2.local>
2020-02-06 17:39:34 -0500

    beginning component for tracking current active route

--- client/src/components/slider.js
+++ client/src/components/slider.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import styled from "styled-components";
 // import PropTypes from 'prop-types'
 
+//reference: https://www.freecodecamp.org/news/how-to-build-a-range-slider-component-in-react-from-scratch-using-only-div-and-span-d53e1a62c4a3/
+//Bernardo-Castilho/dragdroptouch
+//dragdroptouch - Polyfill that enables HTML5 drag drop support on mobile (touch) devices.github.com
+
 const sliderContainer = styled.div`
   position: relative;
 `;
