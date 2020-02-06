7dff0c599623047962f0551f32c94d03ea9f0890
LesliePlumb <lplumb@fa-ev10785sens2.local>
2020-02-06 17:30:41 -0500

    setting up css for slider

--- client/src/components/slider.js
+++ client/src/components/slider.js
@@ -1,5 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
+import styled from "styled-components";
+// import PropTypes from 'prop-types'
 
-export default function slider() {
-  return <div></div>;
+const sliderContainer = styled.div`
+  position: relative;
+`;
+
+const sliderSelectedScale = styled.div``;
+
+const slide = styled.div`
+  position: relative;
+  user-select: none;
+  margin-top: 10px;
+`;
+
+const sliderScale = styled.div``;
+
+const sliderThumb = styled.div`
+  position: absolute;
+  background-color: gray;
+  height: 20px;
+  width: 20px;
+  border-radius: 50%;
+  top: -8px;
+  touch-action: none;
+`;
+
+export class slider extends Component {
+  state = {
+    slots: 24,
+    start: 0,
+    end: 10,
+    labelMode: "mid"
+  };
+
+  render() {
+    return <div></div>;
+  }
 }
+
+export default slider;
