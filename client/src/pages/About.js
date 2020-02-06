25712c95ee2bd9363a69988c04c7c1c1f7383d43
LesliePlumb <lplumb@fa-ev10785sens2.local>
2020-02-06 01:47:15 -0500

    small note for tomorrow

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -3,6 +3,11 @@ import teapot from "../assets/img/pot.jpg";
 // import ReactDOM from "react-dom";
 
 class About extends Component {
+
+  componentDidMount(
+
+    //introduce callback function so that the app and then child nav component will known when each of these components have loaded
+  ) 
   render() {
     return (
       <div>
