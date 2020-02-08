8b13c2e63524f9c9a0938395940f843a6eae9708
LesliePlumb <lplumb@fa-ev10785sens2.local>
2020-02-07 21:34:28 -0500

    installed a react p5 wrapper which is apparently is needed to use p5 in a react environment. the documentation says "The react-p5-wrapper is a wrapper component that receives a reference (instance mode) to the sketch and uses some of the react component "lifecycle methods" to manipulate it accordingly, mainly by executing a method called myCustomRedrawAccordingToNewPropsHandler(props) that needs to be defined within said sketch." https://github.com/and-who/react-p5-wrapper

--- client/src/pages/Patch.js
+++ client/src/pages/Patch.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import patch from "../assets/img/threshold-1.gif";
 
+// import { withTheme } from "styled-components";
+// import styled from "styled-components";
+
 export class Patch extends Component {
   UNSAFE_componentWillMount() {
     this.props.updateLocation(window.location);
@@ -9,7 +12,7 @@ export class Patch extends Component {
   render() {
     return (
       <div>
-        <img src={patch}></img>
+        <img src={patch} alt="threshold"></img>
       </div>
     );
   }
