d6de29df39952cba1a20936304c417c221cdf810
LesliePlumb <lplumb@fa-ev10785sens2.local>
2020-02-08 23:08:27 -0500

    not a solution but now the dots are rendering each time the patches route is visited // i need to unload all that was dynamically appended to the body tag. this is why they are persisting.

--- client/src/components/RandomLinkPlace.js
+++ client/src/components/RandomLinkPlace.js
@@ -31,39 +31,38 @@ const newPosition4Circles = () => {
   return [nh, nw, s];
 };
 
-const target = document.querySelector(".target");
-
-linkList.forEach(function(txt2) {
-  var newLinkDot = document.createElement("div");
-  newLinkDot.className = "dot";
-  newLinkDot.style.backgroundColor = randomColor();
-  var a = document.createElement("a");
-  a.href = "https://convalizards.glitch.me/" + txt2;
-  a.className = "dotlinks";
-  a.appendChild(newLinkDot);
-  document.body.append(a);
-});
-
-const dotpatches = document.querySelectorAll(".dot");
-
-const dotRandPos = () => {
-  dotpatches.forEach(function(patch) {
-    var newCoor4 = newPosition4Circles();
-    patch.style.top = newCoor4[0] + "px";
-    patch.style.left = newCoor4[1] + "px";
-  });
-};
-
 //ok. not ideal. right now because the elements are being dynamically created and appended outside any of the component rendering, they are actually all loading from the beginning.
 //when the route is left, they are all persisting on screen. that's a little ok because it's still an intereseting effect but this will have to be figure out for the picture element version of this, rather then these small pixels
 //the styling also needs to be managed by styled-components in future refactoring
 
 export class RandomLinkPlace extends Component {
-  componentDidMount() {
-    dotRandPos();
-    setInterval(dotRandPos, 5000);
-  }
+  // componentDidMount() {
+  //   setInterval(dotRandPos, 5000);
+  // }
   render() {
+    const target = document.querySelector(".target");
+
+    linkList.forEach(function(txt2) {
+      var newLinkDot = document.createElement("div");
+      newLinkDot.className = "dot";
+      newLinkDot.style.backgroundColor = randomColor();
+      var a = document.createElement("a");
+      a.href = "https://convalizards.glitch.me/" + txt2;
+      a.className = "dotlinks";
+      a.appendChild(newLinkDot);
+      document.body.append(a);
+    });
+
+    const dotpatches = document.querySelectorAll(".dot");
+
+    const dotRandPos = () => {
+      dotpatches.forEach(function(patch) {
+        var newCoor4 = newPosition4Circles();
+        patch.style.top = newCoor4[0] + "px";
+        patch.style.left = newCoor4[1] + "px";
+      });
+    };
+    dotRandPos();
     return (
       <div>
         <div className="target"></div>
