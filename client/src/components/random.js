4f02329241e616226889738bf41bd02284e7ce7d
LesliePlumb <lplumb@fa-ev10785sens2.local>
2020-02-08 20:30:53 -0500

    adding styling for each link element

--- client/src/components/random.js
+++ client/src/components/random.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import styled from "styled-components";
 
 var linkList = [
   "ecologyofvidz.html",
@@ -9,9 +10,20 @@ var linkList = [
   "swerve.html"
 ];
 
-export class random extends Component {
-  chooseElement = assets => assets[Math.floor(Math.random() * assets.length)];
+const dot = styled.div`
+  height: 35px;
+  width: 35px;
 
+  border-radius: 50%;
+  display: inline-block;
+  position: absolute;
+  z-index: 100;
+`;
+
+const chooseElement = assets =>
+  assets[Math.floor(Math.random() * assets.length)];
+
+export class random extends Component {
   render() {
     return <div></div>;
   }
