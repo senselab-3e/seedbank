1dfae6ee9a89ba3f9734c89a5d34e9b8a02ef12b
LesliePlumb <lplumb@fa-ev10785sens2.local>
2020-02-08 20:27:52 -0500

    file format unpdate

--- client/src/components/random.js
+++ client/src/components/random.js
@@ -1,24 +1,20 @@
-import React, {
-    Component
-} from 'react'
+import React, { Component } from "react";
 
-
-var linkList = ["ecologyofvidz.html", "mondayfiles.html", "oz-glob.html", "pink.html", "spincycle.html", "swerve.html"];
-
+var linkList = [
+  "ecologyofvidz.html",
+  "mondayfiles.html",
+  "oz-glob.html",
+  "pink.html",
+  "spincycle.html",
+  "swerve.html"
+];
 
 export class random extends Component {
+  chooseElement = assets => assets[Math.floor(Math.random() * assets.length)];
 
-
-    chooseElement = assets => assets[Math.floor(Math.random() * assets.length)];
-
-    render() {
-        return ( <
-            div >
-
-            <
-            /div>
-        )
-    }
+  render() {
+    return <div></div>;
+  }
 }
 
-export default random
+export default random;
