e9be1c1568bb81efa8a753b3c9e67bd2576349e8
cepholopoddreamz <leslie.plumb@gmail.com>
2020-02-11 18:26:31 +0000

    for github

--- client/src/components/RandomLinkPlace.js
+++ client/src/components/RandomLinkPlace.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-//import styled from "styled-components";
+//import styled from "styled-components""";
 import LinkElementCreator from "./LinkElementCreator";
 
 var linkList = [
