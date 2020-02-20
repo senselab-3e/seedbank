f89c7ded7a5eb2ad662298d488fba062be2ba3df
cepholopoddreamz <leslie.plumb@gmail.com>
2020-02-20 01:40:39 +0000

    new organization of how the eventlist component compiles so that updates from the create event are passed up to event component and down to the eventlist component

--- client/src/pages/events.js
+++ client/src/pages/events.js
@@ -1,17 +1,57 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import EventCreate from '../components/EventCreate';
-import EventList from '../components/EventList';
+import { Link } from "react-router-dom";
+import EventCreate from "../components/EventCreate";
+import EventList from "../components/EventList";
+import axios from "axios";
 
-const EventsPage = () => {
-	return (
-		<div>
-			<EventCreate/>
-			<EventList/>
-			<br/><br/>
-			<Link to='/'>Back to entryway</Link>
-		</div>
-	);
+import React, { Component } from "react";
+
+export class EventsPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      events: []
+    };
+  }
+
+  checkForUpdates = () => {
+    axios
+      .get("/api/events")
+      .then(events => {
+        this.setState({
+          events: events.data
+        });
+      })
+      .catch(err => console.log(err));
+  };
+
+  componentDidMount() {
+    this.checkForUpdates();
+  }
+
+  render() {
+    return (
+      <div>
+        <EventCreate checkForUpdates={this.checkForUpdates} />
+        <EventList events={this.state.events} />
+        <br />
+        <br />
+        <Link to="/">Back to entryway</Link>
+      </div>
+    );
+  }
 }
 
 export default EventsPage;
+
+// const EventsPage = () => {
+// 	return (
+// 		<div>
+// 			<EventCreate/>
+// 			<EventList/>
+// 			<br/><br/>
+// 			<Link to='/'>Back to entryway</Link>
+// 		</div>
+// 	);
+// }
+
+// export default EventsPage;
