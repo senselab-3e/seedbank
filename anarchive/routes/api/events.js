adcea43f2579046a496002430be17e0c6d5531df
cepholopoddreamz <leslie.plumb@gmail.com>
2020-02-26 20:50:00 +0000

    it works!! ehm. its not live updating the list view yet but if you refresh the browser window you see your item is no longer there. everything in my logic was working but i had a semicolon after the .del() and that seems to have been an issue it was always reading the correct id and where to find it but the semicolon may have disturbed the function call on the delete anyways hurray

--- anarchive/routes/api/events.js
+++ anarchive/routes/api/events.js
@@ -1,20 +1,49 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const knex = require('../../db/knex.js');
+const knex = require("../../db/knex.js");
 
 // GET api/events
-router.get('/', (req, res) => {
-	knex('events')
-		.then(events => { res.send(events); })
-		.catch(err => { console.log(err); })
+router.get("/", (req, res) => {
+	knex("events")
+		.then(events => {
+			res.send(events);
+		})
+		.catch(err => {
+			console.log(err);
+		});
 });
 
 // POST api/events
-router.post('/', (req, res) => {
-	knex('events')
-		.insert({ name: req.body.name,
-				  data: '{sponges: ' + req.body.sponges.toString() + '}' })
-		.catch(err => { console.log(err); })
+router.post("/", (req, res) => {
+	knex("events")
+		.insert({
+			name: req.body.name,
+			data: "{sponges: " + req.body.sponges.toString() + "}"
+		})
+		.catch(err => {
+			console.log(err);
+		});
+});
+
+//DELETE api/events
+
+router.delete("/:id", (req, res) => {
+	console.log("AAAAAAAAAA");
+	//   const event = await knex.findById(req.params.id);
+
+	let select = parseInt(req.params.id);
+	console.log(select)
+	knex("events")
+		.where({
+			id: select
+		})
+		.del()
+		.catch(err => {
+			res.status(500).json({
+				status: 'Error :(',
+				error: err
+			})
+		});
 });
 
 module.exports = router;
