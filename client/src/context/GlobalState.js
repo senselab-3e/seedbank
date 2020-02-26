5d027fcbff60e5ff39164552f2d91d911792a9fa
cepholopoddreamz <leslie.plumb@gmail.com>
2020-02-26 23:03:16 +0000

    strange bug encountered when i move from deleting an event to adding an event. if i go from adding an event, to deleting it works fine. but when i go back to adding an event following a delete i stalls out. at first it seems like nothing is happening for about 3-5 min but then i get erros in the console saying err empty response and uncaught in promise can read property data of undefined soooo..... hmm. the value comming from the form submit is coming in as null

--- client/src/context/GlobalState.js
+++ client/src/context/GlobalState.js
@@ -15,6 +15,152 @@ import axios from "axios";
 //   event_end: "2019-09-11"
 // }
 
+//strange bug encountered when i move from deleting an event to adding an event. if i go from adding an event, to deleting it works fine. but when i go back to adding an event following a delete i stalls out. at first it seems like nothing is happening for about 3-5 min but then i get erros in the console saying xhr.js:178 POST http://localhost:3001/api/events net::ERR_EMPTY_RESPONSE
+//terminal 44
+//xhr.js:178 POST http://localhost:3001/api/events net::ERR_EMPTY_RESPONSE
+// (anonymous) @ xhr.js:178
+// e.exports @ xhr.js:12
+// e.exports @ dispatchRequest.js:50
+// Promise.then (async)
+// u.request @ Axios.js:61
+// r.forEach.u.<computed> @ Axios.js:86
+// (anonymous) @ bind.js:9
+// (anonymous) @ GlobalState.js:60
+// u @ runtime.js:45
+// (anonymous) @ runtime.js:271
+// forEach.e.<computed> @ runtime.js:97
+// r @ asyncToGenerator.js:3
+// l @ asyncToGenerator.js:25
+// (anonymous) @ asyncToGenerator.js:32
+// (anonymous) @ asyncToGenerator.js:21
+// s @ GlobalState.js:26
+// addEvent @ GlobalState.js:26
+// onSubmit @ AddEvent.js:35
+// m @ react-dom.production.min.js:15
+// k @ react-dom.production.min.js:15
+// (anonymous) @ react-dom.production.min.js:16
+// S @ react-dom.production.min.js:16
+// O @ react-dom.production.min.js:17
+// P @ react-dom.production.min.js:17
+// N @ react-dom.production.min.js:17
+// En @ react-dom.production.min.js:81
+// ce @ react-dom.production.min.js:285
+// On @ react-dom.production.min.js:83
+// jn @ react-dom.production.min.js:84
+// Nn @ react-dom.production.min.js:84
+// t.unstable_runWithPriority @ scheduler.production.min.js:18
+// Ho @ react-dom.production.min.js:120
+// le @ react-dom.production.min.js:285
+// Pn @ react-dom.production.min.js:82
+// GlobalState.js:68 Uncaught (in promise) TypeError: Cannot read property 'data' of undefined
+//     at GlobalState.js:68
+//     at u (runtime.js:45)
+//     at Generator._invoke (runtime.js:271)
+//     at Generator.forEach.e.<computed> [as throw] (runtime.js:97)
+//     at r (asyncToGenerator.js:3)
+//     at u (asyncToGenerator.js:29)
+// (anonymous) @ GlobalState.js:68
+// u @ runtime.js:45
+// (anonymous) @ runtime.js:271
+// forEach.e.<computed> @ runtime.js:97
+// r @ asyncToGenerator.js:3
+// u @ asyncToGenerator.js:29
+// Promise.then (async)
+// r @ asyncToGenerator.js:13
+// l @ asyncToGenerator.js:25
+// (anonymous) @ asyncToGenerator.js:32
+// (anonymous) @ asyncToGenerator.js:21
+// s @ GlobalState.js:26
+// addEvent @ GlobalState.js:26
+// onSubmit @ AddEvent.js:35
+// m @ react-dom.production.min.js:15
+// k @ react-dom.production.min.js:15
+// (anonymous) @ react-dom.production.min.js:16
+// S @ react-dom.production.min.js:16
+// O @ react-dom.production.min.js:17
+// P @ react-dom.production.min.js:17
+// N @ react-dom.production.min.js:17
+// En @ react-dom.production.min.js:81
+// ce @ react-dom.production.min.js:285
+// On @ react-dom.production.min.js:83
+// jn @ react-dom.production.min.js:84
+// Nn @ react-dom.production.min.js:84
+// t.unstable_runWithPriority @ scheduler.production.min.js:18
+// Ho @ react-dom.production.min.js:120
+// le @ react-dom.production.min.js:285
+// Pn @ react-dom.production.min.js:82
+// xhr.js:178 DELETE http://localhost:3001/api/events/44 net::ERR_EMPTY_RESPONSE
+// (anonymous) @ xhr.js:178
+// e.exports @ xhr.js:12
+// e.exports @ dispatchRequest.js:50
+// Promise.then (async)
+// u.request @ Axios.js:61
+// r.forEach.u.<computed> @ Axios.js:76
+// (anonymous) @ bind.js:9
+// (anonymous) @ GlobalState.js:77
+// u @ runtime.js:45
+// (anonymous) @ runtime.js:271
+// forEach.e.<computed> @ runtime.js:97
+// r @ asyncToGenerator.js:3
+// l @ asyncToGenerator.js:25
+// (anonymous) @ asyncToGenerator.js:32
+// (anonymous) @ asyncToGenerator.js:21
+// i @ GlobalState.js:26
+// deleteEvent @ GlobalState.js:26
+// onClick @ Event.js:8
+// m @ react-dom.production.min.js:15
+// k @ react-dom.production.min.js:15
+// (anonymous) @ react-dom.production.min.js:16
+// S @ react-dom.production.min.js:16
+// O @ react-dom.production.min.js:17
+// P @ react-dom.production.min.js:17
+// N @ react-dom.production.min.js:17
+// En @ react-dom.production.min.js:81
+// ce @ react-dom.production.min.js:285
+// On @ react-dom.production.min.js:83
+// jn @ react-dom.production.min.js:84
+// Nn @ react-dom.production.min.js:84
+// t.unstable_runWithPriority @ scheduler.production.min.js:18
+// Ho @ react-dom.production.min.js:120
+// le @ react-dom.production.min.js:285
+// Pn @ react-dom.production.min.js:82
+// GlobalState.js:86 Uncaught (in promise) TypeError: Cannot read property 'data' of undefined
+//     at GlobalState.js:86
+//     at u (runtime.js:45)
+//     at Generator._invoke (runtime.js:271)
+//     at Generator.forEach.e.<computed> [as throw] (runtime.js:97)
+//     at r (asyncToGenerator.js:3)
+//     at u (asyncToGenerator.js:29)
+// (anonymous) @ GlobalState.js:86
+// u @ runtime.js:45
+// (anonymous) @ runtime.js:271
+// forEach.e.<computed> @ runtime.js:97
+// r @ asyncToGenerator.js:3
+// u @ asyncToGenerator.js:29
+// Promise.then (async)
+// r @ asyncToGenerator.js:13
+// l @ asyncToGenerator.js:25
+// (anonymous) @ asyncToGenerator.js:32
+// (anonymous) @ asyncToGenerator.js:21
+// i @ GlobalState.js:26
+// deleteEvent @ GlobalState.js:26
+// onClick @ Event.js:8
+// m @ react-dom.production.min.js:15
+// k @ react-dom.production.min.js:15
+// (anonymous) @ react-dom.production.min.js:16
+// S @ react-dom.production.min.js:16
+// O @ react-dom.production.min.js:17
+// P @ react-dom.production.min.js:17
+// N @ react-dom.production.min.js:17
+// En @ react-dom.production.min.js:81
+// ce @ react-dom.production.min.js:285
+// On @ react-dom.production.min.js:83
+// jn @ react-dom.production.min.js:84
+// Nn @ react-dom.production.min.js:84
+// t.unstable_runWithPriority @ scheduler.production.min.js:18
+// Ho @ react-dom.production.min.js:120
+// le @ react-dom.production.min.js:285
+// Pn @ react-dom.production.min.js:82
 const initialState = {
   events: [],
   error: null,
