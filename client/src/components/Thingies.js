2fb088be583ceadddf69078533f5d9bea9ca04f6
cepholopoddreamz <leslie.plumb@gmail.com>
2020-02-28 02:30:33 +0000

    continuing to add css styles and further testing if i can pass things to EventPopulate component fairly happily

--- client/src/components/Thingies.js
+++ client/src/components/Thingies.js
@@ -14,6 +14,7 @@ function Thingies() {
     <div>
       <EventPopulate array={events} className={"threshold"} />
       <EventPopulate array={[1, 2, 3, 4, 5, 6]} />
+      <EventPopulate array={[1, 2, 3, 4, 5, 6]} className={"line"} />
     </div>
   );
 }
