3076e9a462add8f9428bce656267411f9735182c
cepholopoddreamz <leslie.plumb@gmail.com>
2020-03-06 00:22:26 +0000

    setting up hooks to grab state for SignIn function component so that the json data from the p5 generated signature can be passed up and back down to the button that will be outside the field of the dynamically drawn canvas so that that data can be discretely passed off to axios and not be interfered with by the way the p5 code has been written to act on mouseRelease in particular... which was cancelling out all other possible actions before

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import teapot from "../pot.jpg";
 
 import Signature from "../components/Signature";
 
 export default function SignIn() {
+  //const [sig, setSig] = useState("");
   return (
     <div>
       <div>
-        <button>adsdsafds</button>
+        <button onClick={() => console.log("clicking")}>CLICKING</button>
         <Signature />
         <img className="Cup" src={teapot} alt="teapot"></img>
       </div>
