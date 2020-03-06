d2cacf860877c9029f973e28f1ced1504775b758
cepholopoddreamz <leslie.plumb@gmail.com>
2020-03-06 00:26:14 +0000

    further setting up hooks for grabbing signature from child components

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -4,12 +4,18 @@ import teapot from "../pot.jpg";
 import Signature from "../components/Signature";
 
 export default function SignIn() {
-  //const [sig, setSig] = useState("");
+  const [sig, setSig] = useState("");
+
+  const grabSignature = data => {
+    setSig(data);
+  };
+
+  console.log(sig);
   return (
     <div>
       <div>
         <button onClick={() => console.log("clicking")}>CLICKING</button>
-        <Signature />
+        <Signature grabSign={grabSignature} />
         <img className="Cup" src={teapot} alt="teapot"></img>
       </div>
     </div>
