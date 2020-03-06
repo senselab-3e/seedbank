7ca71000a6be4649a2ce0d47003e57db5244b600
cepholopoddreamz <leslie.plumb@gmail.com>
2020-03-06 02:13:27 +0000

    adding hooks to setstate in signin component but not settled on approach

--- client/src/pages/SignIn.js
+++ client/src/pages/SignIn.js
@@ -6,6 +6,8 @@ import SendSig from "../components/SendSig";
 
 export default function SignIn() {
   const [sig, setSig] = useState("");
+  const [color, setColor] = useState("");
+  const [path, setPath] = useState("");
 
   const grabSignature = data => {
     setSig(data);
@@ -13,20 +15,35 @@ export default function SignIn() {
 
   const handleChange = props => {
     console.log(props, "handlechange");
-    // if (props !== "") {
-    //   //it's an object so you can't do a simple slice
-    //   // console.log(props.data.path, "afterslice");
-    //   // props.data.path = props.data.path.splice(0, 75);
-    //   // console.log(props.data.path, "checkslice");
-    //   this.setState({
-    //     color: props.color,
-    //     path: props.path,
-    //     logged: true
-    //   });
-    // } else {
-    //   console.log("nothing to log");
-    // }
+    if (props !== "") {
+      setColor(props.color);
+      setPath(props.path);
+      //submit()
+      //it's an object so you can't do a simple slice
+      // console.log(props.data.path, "afterslice");
+      // props.data.path = props.data.path.splice(0, 75);
+      // console.log(props.data.path, "checkslice");
+    } else {
+      console.log("nothing to log");
+    }
   };
+
+  //   submit(e) {
+  //     console.log("sig being sent", e);
+  //     // e.preventDefault();
+  //     // axios
+  //     //   .post("/api/signatures", {
+  //     //    color: this.state.color,
+  //     //     path: this.state.path
+  //     //   })
+  //     //   .then(event => {
+  //     //     console.log("Added signature: " + event);
+  //     //   })
+  //     //   .catch(err => {
+  //     //     console.log(err);
+  //     //   });
+  //     // //this.props.checkForUpdates(); // this was for updating the list view of events - not applicable in this case
+  //   }
 
   //console.log(sig);
   return (
