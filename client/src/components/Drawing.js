5b387c2f0cdc2e709ba0746122842486b6885e5c
cepholopoddreamz <leslie.plumb@gmail.com>
2020-03-19 23:46:19 +0000

    adding p5 and p5wrapper

--- client/src/components/Drawing.js
+++ client/src/components/Drawing.js
@@ -1,28 +1,28 @@
 import React from "react";
-
+var P5Wrapper = require("react-p5-wrapper");
 // function setup() {
 //     createCanvas(710, 400);
 //     background(102);
 //   }
 
-function draw() {
-  stroke(255);
-  if (mouseIsPressed === true) {
-    line(mouseX, mouseY, pmouseX, pmouseY);
-  }
-}
+// function draw() {
+//   stroke(255);
+//   if (mouseIsPressed === true) {
+//     line(mouseX, mouseY, pmouseX, pmouseY);
+//   }
+// }
 
-var canvas = document.createElement("canvas");
+// var canvas = document.createElement("canvas");
 
-canvas.id = "CursorLayer";
-canvas.width = 710;
-canvas.height = 400;
-canvas.style.zIndex = 8;
-canvas.style.position = "absolute";
-canvas.style.border = "1px solid";
+// canvas.id = "CursorLayer";
+// canvas.width = 710;
+// canvas.height = 400;
+// canvas.style.zIndex = 8;
+// canvas.style.position = "absolute";
+// canvas.style.border = "1px solid";
 
-var body = document.getElementsByTagName("body")[0];
-body.appendChild(canvas);
+// var body = document.getElementsByTagName("body")[0];
+// body.appendChild(canvas);
 
 export default function Drawing() {
   return <div></div>;
