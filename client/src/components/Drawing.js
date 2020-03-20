74fa5281c076c544cef6891860b5d45631996c2d
cepholopoddreamz <leslie.plumb@gmail.com>
2020-03-20 01:50:25 +0000

    messing around with draw function

--- client/src/components/Drawing.js
+++ client/src/components/Drawing.js
@@ -12,8 +12,16 @@ export default function Drawing() {
     // // NOTE: Do not use setState in draw function or in functions that is executed in draw function... pls use normal variables or class properties for this purposes
 
     p5.stroke(255);
-    if (p5.mouseIsPressed === true) {
-      p5.line(p5.mouseX, p5.mouseY, p5.pmouseX, p5.pmouseY);
+    //The mouseIsPressed system variable in p5.js is used to store the boolean value. If mouse is pressed then it stores True otherwise store False.
+    ///this is why simply if (p5.mousePressed) is not the right direction
+    if (p5.mouseIsPressed) {
+      //p5.line(p5.mouseX, p5.mouseY, 70, 70);
+      //The system variable pmouseX always contains the horizontal position of the mouse in the frame previous to the current frame.
+      //however, for some reason it's not holding this value for very long or at all.....
+      p5.rect(p5.mouseX, p5.mouseY, p5.pmouseX, p5.pmouseY);
+      p5.ellipse(50, 50, 50, 50);
+    } else {
+      p5.rect(25, 25, 50, 50);
     }
   };
 
