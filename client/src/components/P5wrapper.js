6d63d8293a96b1be3ff5e3a0f6d021486eb083f0
cepholopoddreamz <leslie.plumb@gmail.com>
2020-03-22 23:40:21 +0000

    i have no idea what changed because nothing did but i just reinstalled the library in both the server side json and client json the sketches are back but i dont know still whats going on onwards

--- client/src/components/P5wrapper.js
+++ client/src/components/P5wrapper.js
@@ -1,11 +1,16 @@
 import React from "react";
-//import P5Wrapper from "react-p5-wrapper";
+import P5Wrapper from "react-p5-wrapper";
 
 import sketch3 from "./sketches/sketch3";
 import sketch4 from "./sketches/sketch4";
 class P5wrapper extends React.Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <P5Wrapper sketch={sketch3} />
+        <P5Wrapper sketch={sketch4} />
+      </div>
+    );
   }
 }
 
