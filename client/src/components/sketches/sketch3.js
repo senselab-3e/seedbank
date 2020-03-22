388f541c55a58e1814b67f30d48c331595602669
cepholopoddreamz <leslie.plumb@gmail.com>
2020-03-22 03:42:00 +0000

    great. things are now running as they should this wrapper is great because all the p5 stuff can just stay in its own separate component

--- client/src/components/sketches/sketch3.js
+++ client/src/components/sketches/sketch3.js
@@ -1,20 +1,22 @@
 export default function sketch3(p) {
-    p.setup = function () {
-        p.createCanvas(500, 500)
-        p.background(0);
-    };
+  p.setup = function() {
+    p.createCanvas(500, 500);
+    p.background(0);
+  };
 
-    //function variableEllipse(x, y, px, py, p5) {
-    //     let speed = p5.abs(x - px) + p5.abs(y - py);
-    //     p.stroke(speed);
-    //     p.ellipse(x, y, speed, speed);
-    // }
+  function variableEllipse(x, y, px, py, p) {
+    let speed = p.abs(x - px) + p.abs(y - py);
+    p.stroke(speed);
+    p.ellipse(x, y, speed, speed);
+  }
 
-    p.draw = function () {
-        p.stroke(255);
-        if (p.mouseIsPressed === true) {
-            p.line(p.mouseX, p.mouseY, p.pmouseX, p.pmouseY);
-        }
-
-    };
+  p.draw = function() {
+    p.stroke(255);
+    p.fill(p.pmouseX % 255, p.pmouseY % 255, p.mouseY % 255);
+    if (p.mouseIsPressed === true) {
+      p.line(p.mouseX, p.mouseY, p.pmouseX, p.pmouseY);
+      variableEllipse(p.mouseX, p.mouseY, p.pmouseX, p.pmouseY, p);
+      p.ellipse(150, 150, 50, 50);
+    }
+  };
 }
