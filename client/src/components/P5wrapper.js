b68f3d053b91759175917bdac4f0810e6ba85cd7
cepholopoddreamz <leslie.plumb@gmail.com>
2020-03-22 04:17:30 +0000

    ok something weird happened a the wrapper library directory was lost when i uninstalled the other one and so i had to do a hard reset but now things are working as they should

--- client/src/components/P5wrapper.js
+++ client/src/components/P5wrapper.js
@@ -1,43 +1,13 @@
 import React from "react";
 import P5Wrapper from "react-p5-wrapper";
-import sketch from "./sketches/sketch";
-import sketch2 from "./sketches/sketch2";
+
+import sketch3 from "./sketches/sketch3";
 
 class P5wrapper extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      rotation: 150,
-      stateSketch: sketch
-    };
-  }
-
-  rotationChange(e) {
-    this.setState({ rotation: e.target.value });
-  }
-
-  pressEvent() {
-    this.state.stateSketch === sketch
-      ? this.setState({ stateSketch: sketch2 })
-      : this.setState({ stateSketch: sketch });
-  }
-
   render() {
     return (
       <div>
-        <P5Wrapper
-          sketch={this.state.stateSketch}
-          rotation={this.state.rotation}
-        />
-        <input
-          type="range"
-          value={this.state.rotation}
-          min="0"
-          max="360"
-          step="1"
-          onInput={this.rotationChange.bind(this)}
-        />
-        <button onClick={this.pressEvent.bind(this)}>Change Sketch</button>
+        <P5Wrapper sketch={sketch3} />
       </div>
     );
   }
