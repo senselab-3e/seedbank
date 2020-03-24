ec84470f85129d8175f479c1063f93c6f74dd983
cepholopoddreamz <leslie.plumb@gmail.com>
2020-03-24 06:27:47 +0000

    just messing with jitter timing

--- client/src/components/sketches/sketch8.js
+++ client/src/components/sketches/sketch8.js
@@ -18,16 +18,36 @@ export default function sketch8(p) {
         // during even-numbered seconds (0, 2, 4, 6...) add jitter to
         // the rotation
         if (p.second() % 2 === 0) {
-            jitter = p.random(-0.1, 0.1);
+
+            jitter < 1 ? jitter += 0.0001 : jitter = 0; //p.random(-0.1, 0.1);
         }
         //increase the angle value using the most recent jitter value
         angle = angle + jitter;
         //use cosine to get a smooth CW and CCW motion when not jittering
-        let c = p.cos(angle);
+        let c = p.sin(angle);
         //move the shape to the center of the canvas
         p.translate(p.width / 2, p.height / 2);
         //apply the final rotation
         p.rotate(c);
         p.rect(0, 0, 10, 380);
     }
+
+    // p.draw = function () {
+    //     p.background(51);
+
+    //     // during even-numbered seconds (0, 2, 4, 6...) add jitter to
+    //     // the rotation
+    //     if (p.second() % 2 === 0) {
+    //         jitter = p.random(-0.1, 0.1);
+    //     }
+    //     //increase the angle value using the most recent jitter value
+    //     angle = angle + jitter;
+    //     //use cosine to get a smooth CW and CCW motion when not jittering
+    //     let c = p.cos(angle);
+    //     //move the shape to the center of the canvas
+    //     p.translate(p.width / 2, p.height / 2);
+    //     //apply the final rotation
+    //     p.rotate(c);
+    //     p.rect(0, 0, 10, 380);
+    // }
 }
