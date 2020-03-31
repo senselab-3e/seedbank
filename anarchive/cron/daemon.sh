5b9dc00c6ed4063cb380ddaf1e6fd0a6aa6983a9
Joel Auerbach <joelauerbach1@gmail.com>
2020-03-30 20:34:37 -0400

    Cron path

--- anarchive/cron/daemon.sh
+++ anarchive/cron/daemon.sh
@@ -5,9 +5,9 @@ if [ $status = "online" ]; then
 	echo "Process is running." >/dev/null 2>&1 # can redirect this to console or log file as needed
 elif [ $status = "none" ]; then
 	echo "No processes running." >/dev/null 2>&1
-	npm run start
+	cd /home/senselab/public_html/seedbank/anarchive && npm run start
 else
 	echo "Process is not running." >/dev/null 2>&1
 	pm2 stop server
-	npm run start
+	cd /home/senselab/public_html/seedbank/anarchive && npm run start
 fi
