f923d783aa441af07a0d60ed85828a1cdad9ca7e
cepholopoddreamz <leslie.plumb@gmail.com>
2020-04-11 00:11:00 +0000

    working on different ways of tracking previous positions

--- client/src/components/P5wrapper.js
+++ client/src/components/P5wrapper.js
@@ -1,17 +1,18 @@
 import React from "react";
 import P5Wrapper from "react-p5-wrapper";
-import sketch3 from "./sketches/sketch3";
+// import sketch3 from "./sketches/sketch3";
 //import tutorial from "./sketches/tutorial-repo-sketch";
-import sketch4 from "./sketches/sketch4";
-import sketch5 from "./sketches/sketch5";
-import sketch6 from "./sketches/sketch6";
-import sketch7 from "./sketches/sketch7";
-import sketch8 from "./sketches/sketch8";
-import sketch9 from "./sketches/sketch9";
-import sketch10 from "./sketches/sketch10";
+// import sketch4 from "./sketches/sketch4";
+// import sketch5 from "./sketches/sketch5";
+// import sketch6 from "./sketches/sketch6";
+// import sketch7 from "./sketches/sketch7";
+// import sketch8 from "./sketches/sketch8";
+// import sketch9 from "./sketches/sketch9";
+// import sketch10 from "./sketches/sketch10";
 //import sketch11 from "./sketches/sketch11";
 //import sketch12 from "./sketches/sketch12";
-import sketch13 from "./sketches/sketch13";
+// import sketch13 from "./sketches/sketch13";
+import sketch14 from "./sketches/sketch14";
 
 //import imageprinting from "./sketches/image";
 
@@ -20,18 +21,18 @@ class P5wrapper extends React.Component {
     return (
       <div>
         {/* <P5Wrapper sketch={imageprinting} /> */}
-
-        <P5Wrapper sketch={sketch9} />
-        <P5Wrapper sketch={sketch13} />
-        <P5Wrapper sketch={sketch4} />
-        <P5Wrapper sketch={sketch5} />
+        <P5Wrapper sketch={sketch14} />
+        {/* <P5Wrapper sketch={sketch9} /> */}
+        {/* <P5Wrapper sketch={sketch13} /> */}
+        {/* <P5Wrapper sketch={sketch4} />
+        <P5Wrapper sketch={sketch5} /> */}
         {/* <P5Wrapper sketch={sketch11} /> */}
-        <P5Wrapper sketch={sketch3} />
+        {/* <P5Wrapper sketch={sketch3} />
         <P5Wrapper sketch={sketch10} />
         <P5Wrapper sketch={sketch6} />
 
         <P5Wrapper sketch={sketch7} />
-        <P5Wrapper sketch={sketch8} />
+        <P5Wrapper sketch={sketch8} /> */}
       </div>
     );
   }
