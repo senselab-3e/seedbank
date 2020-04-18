11aa873f58969f7c678e9d1163edc8b3fab8d03d
cepholopoddreamz <leslie.plumb@gmail.com>
2020-04-18 04:35:57 +0000

    adding perlin particles around mouse location

--- client/src/components/sketches/sketch19.js
+++ client/src/components/sketches/sketch19.js
@@ -9,18 +9,22 @@ export default function sketch18(p) {
             this.opacity = 102;
         }
         display() {
-            var x = p.width * p.noise(this.steps);
-            var y = p.height * p.noise(this.steps + 5);
-            var r = 255 * p.noise(this.steps + 10);
-            var g = 255 * p.noise(this.steps + 15);
-            var b = 255 * p.noise(this.steps + 20);
+            console.log(p.mouseX, 'origin')
+            console.log(p.mouseX * p.noise(this.steps + p.random(1, 15)), 'noise');
+            // var x = p.mouseX + p.mouseX * p.noise(this.steps + p.random(0, 10));
+            // var y = p.mouseY + p.mouseY * p.noise(this.steps + p.random(0, 10));
+            var x = p.mouseX * p.noise(this.steps + p.random(1, 5)) * 2;
+            var y = p.mouseY * p.noise(this.steps + p.random(1, 5)) * 2;
+            var r = 255 * p.noise(this.steps);
+            var g = 255 * p.noise(this.steps);
+            var b = 255 * p.noise(this.steps);
 
             p.noStroke();
             p.fill(r, g, b);
             p.ellipse(x, y, this.diam, this.diam);
         }
         update() {
-            this.steps += 0.01;
+            this.steps += 0.1;
 
         }
     }
@@ -30,7 +34,7 @@ export default function sketch18(p) {
         p.createCanvas(500, 500);
         p.background(240);
         for (var i = 0; i < 5; i++) {
-            let newPerlin = new Perlin(p.mouseX, p.mouseY, 5, p.random(0, 10));
+            let newPerlin = new Perlin(p.mouseX, p.mouseY, 5, 1);
             perlins.push(newPerlin)
         }
 
@@ -39,6 +43,7 @@ export default function sketch18(p) {
     let perlins = []
 
     p.draw = function () {
+        p.background(255, 10);
         for (let m = 0; m < perlins.length; m++) {
             perlins[m].display();
             perlins[m].update();
