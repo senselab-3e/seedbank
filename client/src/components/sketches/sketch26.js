d9fdba8101751e96bfb1681e26b0a49f15169a5a
cepholopoddreamz <leslie.plumb@gmail.com>
2020-04-22 21:55:45 +0000

    really basic passing strings through different stylings - p5 is not efficient but trying to learn its logic

--- client/src/components/sketches/sketch26.js
+++ client/src/components/sketches/sketch26.js
@@ -7,14 +7,39 @@ export default function sketch26(p) {
     p.setup = function () {
         p.createCanvas(width, height);
         p.background('teal')
-        p.fill('white')
+
         var sentence = "progress doesn't happen in\na straight line"
-        p.textSize(30);
-        p.text(sentence, 10, 200)
+        //p.textSize(30);
+        // var textstyling = p.textFont('Georgia', 30)
+        // p.textFont(textstyling)
+        // p.text(sentence, 10, 200)
+
+        var x = 10;
+
+        console.log(sentence.charAt(1))
+
+        for (let i = 0; i < sentence.length; i++) {
+            let letter = sentence.charAt(i)
+            console.log(sentence[i])
+            p.textSize(p.random(12, 48));
+            p.text(letter, x, 300)
+            // p.text(sentence[i], x, 400);
+            // p.text(sentence.charAt(i), x, 300); //smooshes everything together
+            //x += 35;
+            x += p.textWidth(letter)
+        }
 
 
+
+        // for (let i = 0; i = sentence.length; i++) {
+        //     console.log(sentence.length)
+        //     //p.text(sentence[i], x, 300);
+        //     //p.text(sentence, x, 200)
+        //     //x += 32;
+        // }
 
     }
+
 
     p.draw = function () {
 
