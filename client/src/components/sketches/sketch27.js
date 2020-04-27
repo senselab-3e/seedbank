09d10b0fc6e1fa24fa912844e879aca96f2b3684
cepholopoddreamz <leslie.plumb@gmail.com>
2020-04-27 01:06:49 +0000

    adding way to get static random numbers aka instead of random being called at every frame render

--- client/src/components/sketches/sketch27.js
+++ client/src/components/sketches/sketch27.js
@@ -12,35 +12,66 @@ export default function sketch27(p) {
         p.rectMode(p.CENTER);
         p.angleMode(p.DEGREES);
 
-        var radius2 = 50;
 
-        p.push();
-        p.translate(width / 2, height / 2);
-
-        for (var n = 0; n < 15; n++) {
-            p.rotate(n);
-
-            var letter = new Letter('word', 0 + radius2, 0, 90, coloring);
-            letter.render();
-        }
-        p.pop();
 
     }
 
-    var coloring = p.color(p.random(255), p.random(255), p.random(255))
+    //By default, random() produces different results each time the program is run. Set the seed parameter to a constant to return the same pseudo-random numbers each time the software is run.
+
+
 
     p.draw = function () {
         p.background('teal')
-        let radius = 0;
-
-        p.mouseX < 200 ? radius = p.mouseX : radius = 200;
+        let radius = 100;
+        const amount = 15;
+        const spacing = 360 / amount; //p.mouseX / amount;
+        var coloring = p.color(p.random(255), p.random(255), p.random(255))
+        // p.mouseX < 10 ? radius = 10 : radius = p.mouseX;
+        // p.mouseX < 200 ? radius = p.mouseX : radius = 200;
         p.push();
         p.translate(width / 2, height / 2);
 
-        for (var i = 0; i < 15; i++) {
-            p.rotate(i);
+
+        //use the push and pop so that the transformation effects remain localized and don’t accumulate.
+        for (let i = 0; i < amount; i++) {
+            p.push();
+            p.rotate(i * spacing);
             var num = new Num(1, 0 + radius, 0, 90, 255);
             num.render();
+            p.pop();
+        }
+        p.pop();
+
+        var radius2 = 50;
+        var amount2 = 5;
+
+        createWordRing(radius2, amount2, 1);
+    }
+
+    function createWordRing(radius2, amount2, seed) {
+        p.randomSeed(seed)
+        var randomNumbers = [];
+        for (let i = 0; i <= amount2; i++) {
+            randomNumbers.push({
+                r: parseInt(p.random(255)),
+                b: parseInt(p.random(255)),
+                g: parseInt(p.random(255))
+            })
+        }
+
+        const spacing2 = 360 / amount2;
+        console.log(randomNumbers[0], randomNumbers[1])
+
+        p.push();
+        p.translate(width / 2, height / 2);
+
+        for (var n = 0; n < amount2; n++) {
+            p.push();
+            p.rotate(n * spacing2);
+            var letter = new Letter('word', 0 + radius2, 0, 90);
+            letter.render();
+            letter.display()
+            p.pop();
         }
         p.pop();
 
@@ -75,11 +106,12 @@ export default function sketch27(p) {
             this.r = p.random(255)
             this.g = p.random(255)
             this.b = p.random(255)
+            this.clr = clr
         }
 
         render() {
             p.push();
-            p.fill(this.r, this.g, this.b);
+            //p.fill(this.clr);
             p.translate(this.x, this.y);
             p.rotate(this.rot);
             p.text(this.msg, 0, 0);
@@ -87,7 +119,8 @@ export default function sketch27(p) {
         }
 
         display() {
-
+            //sconsole.log(this.clr)
+            //p.fill(this.clr);
         }
     }
 
