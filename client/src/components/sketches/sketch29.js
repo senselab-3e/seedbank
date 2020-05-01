e67026a026143ec6c660306279266b332a9ef928
cepholopoddreamz <leslie.plumb@gmail.com>
2020-05-01 21:37:36 +0000

    just practicing 1d logic for noise

--- client/src/components/sketches/sketch29.js
+++ client/src/components/sketches/sketch29.js
@@ -1,5 +1,8 @@
 export default function sketch28(p) {
 
+    var width = 500
+    var height = 500
+
     var xoff1 = 0;
     var xoff2 = 10000
     //noise doesn't have a uniform distribution /// and it's usually a point value between 0 and 1. 
@@ -7,7 +10,7 @@ export default function sketch28(p) {
     //if you call noise(100) you will always get the same value. it's calling a point in time. 
     p.setup = function () {
         p.createCanvas(500, 500);
-        p.background('black')
+        //p.background('black')
 
         // p.textAlign(p.CENTER, p.CENTER);
         // p.rectMode(p.CENTER);
@@ -17,17 +20,32 @@ export default function sketch28(p) {
     p.draw = function () {
 
         p.background(51);
+        p.stroke(255);
+        p.noFill();
 
-        // var x = p.random(p.width)
-        //this is tying x positions of the ellipse to perlin noise values over time
-        var x = p.map(p.noise(xoff1), 0, 1, 0, p.width)
-        var y = p.map(p.noise(xoff2), 0, 1, 0, p.width)
-        //could also multiply it by the width
-        xoff2 += 0.02
-        xoff1 += 0.01 // important -- it ties the previous number with the next, that can still be seen as relational.
-        //if the offsets are the  same, then the same position in time is being referenced by the noise funciton -- so the ellipse will just keep moving along a diagonal
-        p.ellipse(x, y, 24, 24);
+        // //this is tying x positions of the ellipse to perlin noise values over time
+        // var x = p.map(p.noise(xoff1), 0, 1, 0, p.width)
+        // var y = p.map(p.noise(xoff2), 0, 1, 0, p.width)
+        // //could also multiply it by the width
+        // xoff2 += 0.02
+        // xoff1 += 0.01 // important -- it ties the previous number with the next, that can still be seen as relational.
+        // //if the offsets are the  same, then the same position in time is being referenced by the noise funciton -- so the ellipse will just keep moving along a diagonal
+        // p.ellipse(x, y, 24, 24);
+        p.beginShape();
+        for (let x = 0; x < width; x++) {
+            p.stroke(255)
+            let ranRad = p.floor(p.random(15, 75))
+            //p.point(x, p.random(height));
+            p.vertex(x, p.random(height))
+            p.push()
+            p.noStroke()
+            p.fill(p.random(255), p.random(255), p.random(255))
+            p.ellipse(x, p.random(height), ranRad, ranRad)
 
+            p.pop()
+        }
+        p.endShape()
+        p.noLoop()
 
     }
 
