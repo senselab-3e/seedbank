9a955ed65a23266a5c63dc7b367affa3aaf05ebc
cepholopoddreamz <leslie.plumb@gmail.com>
2020-05-05 06:10:16 +0000

    progress

--- client/src/components/sketches/sketch33.js
+++ client/src/components/sketches/sketch33.js
@@ -6,7 +6,7 @@ export default function sketch33(p) {
     var height = 500
     let planet
 
-    let colorOptions = ['yellow', 'orange', 'lightblue', 'lightgreen', 'magenta', 'deeppink', 'purple']
+    let colorOptions = ['yellow', 'orange', 'cornflowerblue', 'lightgreen', 'magenta', 'navy', 'lightpurple']
 
     let drawings = []
     let pos2
@@ -16,15 +16,12 @@ export default function sketch33(p) {
 
         p.createCanvas(width, height);
         p.background('grey');
-        planet = new Orbit(0, 0, 14, 'teal', 12)
+        planet = new Orbit(0, 0, p.random(1, 15), 'deeppink', 12)
         drawings.push(planet)
-
         pos2 = p.createVector(width / 2, height / 2);
         let vel = p5.Vector.random2D() // this gives a unit vector and it is 1. i then scale it up from one. random direction /// static function
         vel.mult(5)
         prev = pos2.copy(); // copy creates a new vector
-
-
     }
 
 
@@ -34,6 +31,8 @@ export default function sketch33(p) {
         //p.background('grey');
         // drawing3.update()
         // drawing3.show()
+
+        //this is the more basic mess to figure out the logic of how to draw the vector lines between prev and current position. it is then cleaned up and extracted into the Orbit constructor
         p.push()
         p.stroke('yellow')
         p.strokeWeight(1)
@@ -56,11 +55,11 @@ export default function sketch33(p) {
     p.mousePressed = function () {
 
         if (drawings.length < 20) {
-            planet = new Orbit(p.mouseX, p.mouseY, p.floor(p.random(15, 32)), p.random(colorOptions), p.floor(p.random(3, 32)))
+            planet = new Orbit(p.mouseX, p.mouseY, p.floor(p.random(1, 5)), p.random(colorOptions), p.floor(p.random(3, 32)))
             drawings.push(planet)
         } else {
             drawings.splice(0, 1);
-            planet = new Orbit(p.mouseX, p.mouseY, p.floor(p.random(15, 32)), p.random(colorOptions), p.floor(p.random(3, 32)))
+            planet = new Orbit(p.mouseX, p.mouseY, p.floor(p.random(1, 5)), p.random(colorOptions), p.floor(p.random(3, 32)))
             drawings.push(planet)
 
         }
@@ -73,7 +72,6 @@ export default function sketch33(p) {
         constructor(x, y, size, color, limit) {
 
             this.pos = p.createVector(x, y)
-
             this.size = size;
             this.color = color
             this.limitNum = limit
@@ -94,14 +92,20 @@ export default function sketch33(p) {
             // this.pos.add(this.vel)
 
             //
+
             let mouse = p.createVector(p.mouseX, p.mouseY);
-            //this.prev = mouse.set()
+            // this.prev.set(mouse) /// this value is continually being retreived so i can use it to update the 'previous position before acceleration and velocity or applied.... 
+            //this.prev = mouse.set()   /// this will create this kind of swinging pendulum line drawing from current mouse position and the inertia position calulated later for the pos value
+            this.prev.set(this.pos)
             this.acc = p5.Vector.sub(mouse, this.pos);
             this.acc.setMag(1);
 
             this.vel.add(this.acc);
             this.vel.limit(this.limitNum); // max radius from mouse
             this.pos.add(this.vel);
+            // console.log(this.pos.x, this.prev.x, this.prev.x, this.prev.y)
+
+            // console.log(this.pos.x, this.prev.x, this.prev.x, this.prev.y)
             //new
 
         }
@@ -112,20 +116,20 @@ export default function sketch33(p) {
             // p.stroke(this.color)
             // p.point(this.pos.x, this.pos.y, this.size)
             //p.strokeWeight(5)
-            p.push()
-            p.noStroke()
-            p.fill(this.color)
-            p.ellipse(this.pos.x, this.pos.y, this.size)
-            p.pop()
+
+            // p.push()
+            // p.noStroke()
+            // p.fill(this.color)
+            // p.ellipse(this.pos.x, this.pos.y, this.size)
+            // p.pop()
 
             p.push()
-            p.stroke(255)
-            p.noFill()
-            p.strokeWeight(1)
+
+            //p.fill(this.color)
+            p.strokeWeight(this.size)
             p.stroke(this.color)
-            console.log(this.pos.x, this.prev.x, this.prev.x, this.prev.y)
-
             p.line(this.pos.x, this.pos.y, this.prev.x, this.prev.y)
+            // this.prev.set(this.pos)
             p.pop()
             // p.beginShape()
             // p.vertex(this.prev.x, this.prev.y);
@@ -210,3 +214,77 @@ export default function sketch33(p) {
 
 
 }
+
+//>> STASHING THIS VERSION
+
+// class Orbit {
+//     constructor(x, y, size, color, limit) {
+
+//         this.pos = p.createVector(x, y)
+
+//         this.size = size;
+//         this.color = color
+//         this.limitNum = limit
+//         this.vel = p5.Vector.random2D() // this gives a unit vector and it is 1. i then scale it up from one. random direction /// static function
+//         this.vel.mult(5) //this.vel.mult(p.random(3)) //random velocity between 0 and 3 // this is a scalar multiplier  /// mult function is called on v
+//         this.prev = this.pos.copy()
+
+//         //this.acc.setMag(0.01)
+//     }
+
+//     //limit will cap the vector at a certain amount. // the acceleration accumulates over time // so you can use the limit so that it will accelerate up until a certain limit
+
+//     update() {
+//         //random walk with acceleration
+//         // this.acc = p5.Vector.random2D()
+//         // this.vel.add(this.acc) // adding x,y
+//         // this.vel.limit(2)
+//         // this.pos.add(this.vel)
+
+//         //
+
+//         let mouse = p.createVector(p.mouseX, p.mouseY);
+//         this.prev.set(mouse) /// this value is continually being retreived so i can use it to update the 'previous position before acceleration and velocity or applied.... 
+//         //this.prev = mouse.set()
+//         this.acc = p5.Vector.sub(mouse, this.pos);
+//         this.acc.setMag(1);
+
+//         this.vel.add(this.acc);
+//         this.vel.limit(this.limitNum); // max radius from mouse
+//         this.pos.add(this.vel);
+//         //new
+
+//     }
+
+//     show() {
+//         //p.stroke(255)
+//         // p.strokeWeight(5)
+//         // p.stroke(this.color)
+//         // p.point(this.pos.x, this.pos.y, this.size)
+//         //p.strokeWeight(5)
+
+//         // p.push()
+//         // p.noStroke()
+//         // p.fill(this.color)
+//         // p.ellipse(this.pos.x, this.pos.y, this.size)
+//         // p.pop()
+
+//         p.push()
+
+//         //p.fill(this.color)
+//         p.strokeWeight(1)
+//         p.stroke(this.color)
+//         console.log(this.pos.x, this.prev.x, this.prev.x, this.prev.y)
+
+//         p.line(this.pos.x, this.pos.y, this.prev.x, this.prev.y)
+//         // this.prev.set(this.pos)
+//         p.pop()
+//         // p.beginShape()
+//         // p.vertex(this.prev.x, this.prev.y);
+//         // p.vertex(this.pos.x, this.pos.y);
+//         // p.endShape()
+//         // this.prev.set(this.pos)
+//     }
+
+
+// }
