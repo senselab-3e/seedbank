ac4cac459944daf1d54f6ed3c528501295b41615
cepholopoddreamz <leslie.plumb@gmail.com>
2020-05-13 18:23:16 +0000

    adding framerate tracker

--- client/src/components/sketches/sketch30-perlinnoisefield.js
+++ client/src/components/sketches/sketch30-perlinnoisefield.js
@@ -6,9 +6,11 @@ export default function sketch30(p) {
     // var xoff1 = 0;
     // var xoff2 = 10000
 
-    var inc = 0.01; // the smaller the inc the softer the transitions
-    var scl = 20;
+    var inc = 0.1; // the smaller the inc the softer the transitions
+    var scl = 10;
     var cols, rows;
+
+    var fr;
 
     //NOTES: Sets the pixel scaling for high pixel density displays. By default pixel density is set to match display density, call pixelDensity(1) to turn this off.
 
@@ -16,11 +18,13 @@ export default function sketch30(p) {
         p.createCanvas(width, height);
         p.pixelDensity(1)
         cols = p.floor(width / scl);
-        rows = p.floor(height / scl)
+        rows = p.floor(height / scl);
+        fr = p.createP(''); // creates paragraph element
     }
     p.draw = function () {
         var yoff = 0;
         // p.loadPixels();
+        p.noStroke()
         p.noiseDetail(2, 0.7)
         for (var y = 0; y < rows; y++) {
             var xoff = 0;
@@ -28,14 +32,14 @@ export default function sketch30(p) {
                 var index = (x + y * width) * 4;
                 // var r = p.random(255)
                 var r = p.noise(xoff, yoff) * 255;
-                p.fill(p.random(255))
+                p.fill(r)
                 xoff += inc;
                 p.rect(x * scl, y * scl, scl, scl)
             }
             yoff += inc;
         }
 
-
+        fr.html(p.floor(p.frameRate()))
 
         // p.updatePixels() //key
         //p.noLoop()
