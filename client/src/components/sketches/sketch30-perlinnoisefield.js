b7d09f40195410bf0296076f4c08444854bc7f37
cepholopoddreamz <leslie.plumb@gmail.com>
2020-05-13 18:38:53 +0000

    assigning vector angle to each grid position

--- client/src/components/sketches/sketch30-perlinnoisefield.js
+++ client/src/components/sketches/sketch30-perlinnoisefield.js
@@ -1,3 +1,5 @@
+import p5 from "react-p5-wrapper/node_modules/p5";
+
 export default function sketch30(p) {
 
     var width = 500
@@ -23,18 +25,27 @@ export default function sketch30(p) {
     }
     p.draw = function () {
         var yoff = 0;
-        // p.loadPixels();
-        p.noStroke()
-        p.noiseDetail(2, 0.7)
+        p.randomSeed(10)
+        p.background(255)
         for (var y = 0; y < rows; y++) {
+
+
             var xoff = 0;
             for (var x = 0; x < cols; x++) {
                 var index = (x + y * width) * 4;
                 // var r = p.random(255)
                 var r = p.noise(xoff, yoff) * 255;
-                p.fill(r)
+                //instead of fill, create a vector
+                var v = p5.Vector.fromAngle(p.random(p.TWO_PI))
+                // p.fill(r)
                 xoff += inc;
-                p.rect(x * scl, y * scl, scl, scl)
+                //p.rect(x * scl, y * scl, scl, scl)
+                p.stroke(0)
+                p.push()
+                p.translate(x * scl, y * scl) /// continuously resets the 00 to the particular scl point  being drawn with
+                p.rotate(v.heading())
+                p.line(0, 0, scl, 0)
+                p.pop()
             }
             yoff += inc;
         }
