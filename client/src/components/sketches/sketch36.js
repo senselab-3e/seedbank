15fa263f6ee5fb3a8a9138f41e44349fb0f89447
cepholopoddreamz <leslie.plumb@gmail.com>
2020-05-13 01:31:14 +0000

    testing out repellers

--- client/src/components/sketches/sketch36.js
+++ client/src/components/sketches/sketch36.js
@@ -1,57 +1,5 @@
 import p5 from "react-p5-wrapper/node_modules/p5";
 
-//An object at rest and an object in motion stays in motion 
-//at a constant spped and direction UNLESS acted pone by an UNBALANCED force. 
-// >>do al the net forces on an object add up to a non-zero.
-// a force is a vector that causes an object with mass to accelerate. 
-
-//3rd law : Force always occurs in pairs. The two forces are of equal strength, but in opposite directions
-
-// The mass of an object is a measure of the amount of matter in the object (measured in kilograms).
-// Weight, though often mistaken for mass, is technically the force of gravity on an object. From Newton’s second law, we can calculate it as mass times the acceleration of gravity (w = m * g). Weight is measured in newtons.
-// Density is defined as the amount of mass per unit of volume (grams per cubic centimeter, for example).
-
-
-//a Force is a vector that causes and object with mass, to accelerate
-
-// force = mass * acceleration  // voide update
-//standard
-///////acc = force!
-//vel.add(acc)
-//loc.add(vel)
-
-// Force = Mass * Acceleration
-// A = F/M
-//A = F 
-
-//if Mass = 1
-//A = F!
-
-
-// in coding land we are assuming a mass = 1 so Force = Mass * acceleration meaning 1 * acceleration meaning Force = acceleration
-///--->>key A = F/M  force divided by mass. if i calculate that i can build of previous code that has
-// vel.add(acc)
-//pos.add(vel) /// velocity is the change in position over time. 
-
-// Friction  = -1 * u * N * ˆv -- velocity unit vector
-//-1 is the direction of velocity - tied to the ˆv  unit vector pointing in the direction of unit negative one
-// i need to figure out direction and magnitude
-//what is the magnitude of the friction force -- which is the the opposite direction of the velocity. 
-// the co-efficient of friction = u  
-// the strenght of the friction force, based on the material it is. 
-//Normal force - the 'N' - is the force perpendicular to the two surfaces in contact
-//it's mag is tied force and material
-//in code land the normal force points up. a constant -- it's not dependent on the weight/dependent on the mass --- but in the case of one particular object i can treat it as constant. 
-
-//Friction force was not proportional to the speed (the magnitude) -- where as drag, is proportional. If you hold something still, there is no drag force on it.
-
-//Drag force = -1 * Constantofdrag * velocitysquared * ˆv -- velocity unit vector
-
-
-// when calculating a force i need direction , and magnitude. 
-//so the direction is just an arrow point from the object to the attractor 
-// i can get the attractor position using sub()
-//position of the attractor minus the position of the object
 
 export default function sketch35(p) {
 
@@ -65,6 +13,7 @@ export default function sketch35(p) {
     let movers = []
     let dragC = 0.2
     let attractor;
+    let repeller;
 
 
 
@@ -86,7 +35,8 @@ export default function sketch35(p) {
 
         }
 
-        attractor = new Attractor(width / 2, height / 2, 100);
+        //attractor = new Attractor(width / 2, height / 2, 100);
+        repeller = new Repel(width / 2, height / 2, 100);
         //
         p.background(0);
 
@@ -94,7 +44,7 @@ export default function sketch35(p) {
 
     p.draw = function () {
 
-        p.background(0);
+        //p.background(0);
 
         for (let mover of movers) {
 
@@ -103,14 +53,19 @@ export default function sketch35(p) {
                 //temporarily disabing wind function
                 // let wind = p.createVector(0.1, 0);
                 // mover.applyForce(wind);
-                attractor.pos.x = p.mouseX;
-                attractor.pos.y = p.mouseY;
-
-
+                // attractor.pos.x = p.mouseX;
+                // attractor.pos.y = p.mouseY;
+                repeller.pos.x = p.mouseX;
+                repeller.pos.y = p.mouseY;
+                p.clear();
+                p.background(0);
 
             }
-            attractor.attract(mover);
-            attractor.show();
+            // attractor.attract(mover);
+            // attractor.show();
+
+            repeller.repel(mover);
+            repeller.show();
 
             let gravity = p.createVector(0, 0.2);
             let weight = p5.Vector.mult(gravity, mover.mass)
@@ -146,6 +101,7 @@ export default function sketch35(p) {
             //this.radius = 16;
             this.mass = mass;
             this.radius = p.sqrt(this.mass) * 10 // this is the math for calculating the area oa circle -- rather then just equating the radius to th emass size more arbitraliarily
+            this.prev = this.pos.copy()
 
         }
 
@@ -192,7 +148,7 @@ export default function sketch35(p) {
             // let mouse = createVector(mouseX, mouseY);
             // this.acc = p5.Vector.sub(mouse, this.pos);
             // this.acc.setMag(0.1);
-
+            this.prev.set(this.pos)
             this.vel.add(this.acc);
             this.pos.add(this.vel);
             this.acc.set(0, 0); // this is me clearing the force out at the end of each animatoin cycle
@@ -202,7 +158,8 @@ export default function sketch35(p) {
             p.stroke(255);
             p.strokeWeight(2);
             p.fill(this.color);
-            p.ellipse(this.pos.x, this.pos.y, this.radius * 2);
+            p.line(this.pos.x, this.pos.y, this.prev.x, this.prev.y)
+            //p.ellipse(this.pos.x, this.pos.y, this.radius * 2);
         }
 
         friction() {
@@ -260,7 +217,7 @@ export default function sketch35(p) {
             let force = p5.Vector.sub(this.pos, mover.pos); //calculate the mover and the attractor
             // keep in mind this.pos and mover.pos both contain x,y --- so it is subtracting this.pos.x from mover.pos.x and this.pos.y and mover.pos.y 
             let distanceSq = p.constrain(force.magSq(), 100, 1000); //calculate alll the elements to calculate the magnitude of that vector.
-            let G = 1; //-0.005;
+            let G = 2. //-0.005;
             let strength = (G * (this.mass * mover.mass)) / distanceSq; /// put all of that calculation into a variable called strength
             force.setMag(strength);
             // and then set the magnitude of that vector to stength
@@ -274,8 +231,28 @@ export default function sketch35(p) {
         }
     }
 
+    class Repel {
+        constructor(x, y, m) {
+            this.pos = p.createVector(x, y);
+            this.mass = m;
+            this.r = p.sqrt(this.mass) * 2;
+        }
 
+        repel(mover) {
+            let force = p5.Vector.sub(this.pos, mover.pos); //calculate the mover and the attractor
+            // keep in mind this.pos and mover.pos both contain x,y --- so it is subtracting this.pos.x from mover.pos.x and this.pos.y and mover.pos.y 
+            let distanceSq = p.constrain(force.magSq(), 100, 1000); //calculate alll the elements to calculate the magnitude of that vector.
+            let G = -0.5;
+            let strength = (G * (this.mass * mover.mass)) / distanceSq; /// put all of that calculation into a variable called strength
+            force.setMag(strength);
+            // and then set the magnitude of that vector to stength
+            mover.applyForce(force); /// and then apply that force --- where the applyForce function add the xy amounts to increase the movements
+        }
 
-
-
+        show() {
+            p.noStroke();
+            p.fill(255, 0, 100);
+            p.ellipse(this.pos.x, this.pos.y, this.r * 2);
+        }
+    }
 }
