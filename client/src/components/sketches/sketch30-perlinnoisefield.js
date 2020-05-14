1e7c6c6d0d39a853b331502f9b3a0f59a733818b
cepholopoddreamz <leslie.plumb@gmail.com>
2020-05-14 01:53:48 +0000

    troubleshot particle generator

--- client/src/components/sketches/sketch30-perlinnoisefield.js
+++ client/src/components/sketches/sketch30-perlinnoisefield.js
@@ -32,25 +32,25 @@ export default function sketch30(p) {
     }
     p.draw = function () {
         var yoff = 0;
-        p.randomSeed(10)
+        //p.randomSeed(10)  /// this was for the vector line angle tiling
         p.background(255)
         for (var y = 0; y < rows; y++) {
             var xoff = 0;
             for (var x = 0; x < cols; x++) {
                 var index = (x + y * width) * 4;
                 // var r = p.random(255)
-                var r = p.noise(xoff, yoff, zoff) * p.TWO_PI; //if i want the greyscale then i *255 
+                var angle = p.noise(xoff, yoff, zoff) * p.TWO_PI; //if i want the greyscale then i *255 
                 //instead of fill, create a vector
-                var v = p5.Vector.fromAngle(r)
+                var v = p5.Vector.fromAngle(angle)
                 // p.fill(r)
                 xoff += inc;
                 //p.rect(x * scl, y * scl, scl, scl)
-                p.stroke(0)
-                p.push()
-                p.translate(x * scl, y * scl) /// continuously resets the 00 to the particular scl point  being drawn with
-                p.rotate(v.heading())
-                p.line(0, 0, scl, 0)
-                p.pop()
+                //p.stroke(0)
+                // p.push()
+                // p.translate(x * scl, y * scl) /// continuously resets the 00 to the particular scl point  being drawn with
+                // p.rotate(v.heading())
+                // p.line(0, 0, scl, 0)
+                // p.pop()
             }
             yoff += inc;
             zoff += 0.0005;
@@ -58,13 +58,14 @@ export default function sketch30(p) {
 
         fr.html(p.floor(p.frameRate()))
 
-        p.push()
+        //p.push()
 
         for (let n = 0; n < particles.length; n++) {
             particles[n].show()
             particles[n].update()
+            particles[n].edges()
         }
-        p.pop()
+        //p.pop()
         // p.updatePixels() //key
         //p.noLoop()
     }
@@ -72,7 +73,7 @@ export default function sketch30(p) {
     class Particle {
         constructor(x, y, color) {
             this.pos = p.createVector(p.random(width), p.random(height));
-            this.vel = p.createVector(0, 0);
+            this.vel = p5.Vector.random2D()
             this.acc = p.createVector(0, 0);
         }
 
@@ -95,6 +96,26 @@ export default function sketch30(p) {
             p.point(this.pos.x, this.pos.y)
 
         }
+
+        edges() {
+            if (this.pos.x > width) {
+                this.pos.x = 0;
+                //this.updatePrev();
+            }
+            if (this.pos.x < 0) {
+                this.pos.x = width;
+                //this.updatePrev();
+            }
+            if (this.pos.y > height) {
+                this.pos.y = 0;
+                //this.updatePrev();
+            }
+            if (this.pos.y < 0) {
+                this.pos.y = height;
+                //this.updatePrev();
+            }
+        };
+
     }
 }
 
