3c91e78faedae9e00bfc381ea34e5a4d60bcece1
cepholopoddreamz <leslie.plumb@gmail.com>
2020-05-20 04:17:08 +0000

    colorchange

--- client/src/components/sketches/sketch39.js
+++ client/src/components/sketches/sketch39.js
@@ -14,12 +14,13 @@ export default function sketch39(p) {
     p.draw = function () {
         p.background(255)
         const x = p.width;
-        const dia = 75;
-        const num = 100;
+        const dia = 45;
+        const num = 25;
         p.translate(p.width / 2, p.height / 2)
         for (let a = 0; a < 360; a += 22.5) { //the value i put at a+= decides at what placement each line of dots is drawn at a angle from the centerpoint. 
             p.rotate(p.radians(a));
             p.push()
+            p.fill(10, 197, 190)
             for (let i = 0; i < num; i++) {
                 p.scale(0.9)
                 p.rotate(p.radians(angle));
@@ -27,6 +28,7 @@ export default function sketch39(p) {
             }
             p.pop() // this push and pop is super key, it means that each time a new a value += 22.5 passes through it 'pops' out the drawing for it. not included, then you'll only have a single line of the ellipses drawn out and rotating
             p.push()
+            p.fill(106, 244, 226)
             for (let i = 0; i < num; i++) {
                 p.scale(0.9)
                 p.rotate(-p.radians(angle));
