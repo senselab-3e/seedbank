bb0c6e32a7bcf1c252db621e1fa4f17ac1ed0e42
cepholopoddreamz <leslie.plumb@gmail.com>
2020-05-20 05:23:12 +0000

    adjusted numbers that the vector lines were drawing out from so that they would proportionally change to the canvas size rather then being static values

--- client/src/components/sketches/sketch41.js
+++ client/src/components/sketches/sketch41.js
@@ -8,13 +8,14 @@ export default function sketch41(p) {
     p.draw = function () {
         p.translate(p.width / 2, p.height / 2)
         for (let a = 0; a < 360; a += 1) {
-            let p1 = p.random(50, 150);
-            let p2 = p.random(150, 350);
+            let p1 = p.random(p.width / 8, p.width / 4);
+            let p2 = p.random(p.width / 4, p.width * 0.4);
+            let color = p.color(p.random(255), p.random(255), p.random(255), p.random(50, 255))
             p.push()
             p.rotate(p.radians(a));
             p.strokeCap(p.SQUARE);
             p.strokeWeight(2);
-            p.stroke(40);
+            p.stroke(color);
             p.line(p1, 0, p2, 0);
             p.pop()
         }
