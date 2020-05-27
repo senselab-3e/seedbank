30895b0ed55328982ed7d28ceb9a3d2b448e3166
cepholopoddreamz <leslie.plumb@gmail.com>
2020-05-27 02:28:59 +0000

    added some noise to each vertex to give it a nice little wiggle and flow for each triangulation

--- client/src/components/sketches/sketch3.js
+++ client/src/components/sketches/sketch3.js
@@ -23,6 +23,8 @@ export default function sketch3(p) {
     let xpos = 0; /// this needed to sit outside the draw function. 
     let ypos = 0;
 
+    let noiseScale = 0.002
+
     p.draw = function () {
         // when the background isn't drawn you see all the trace movements of the previous drawings
         p.noFill();
@@ -30,94 +32,43 @@ export default function sketch3(p) {
         //p.stroke(255);
         p.strokeWeight(2);
         p.background("orange");
-        //frame count can only grow linearly so it's not super helpful for animations
-        //p.ellipse(p.frameCount, 200, 100, 50 + (p.frameCount / 2));
-        // p.ellipse(p.frameCount, 200, 100, 50 + (p.frameCount % 30) * 10, 50);
-        //p.ellipse(width / 2, height / 2, (p.frameCount % 30) * 5, 160);
-        //ellipse(200, 200, 100+(sin(frameCount)*50), 100);
-        ///To make the oscillation slower, we need to divide the frameCount value by some other value. Let’s make it ten times slower by dividing by ten:
-        // p.ellipse(width / 2, height / 2, 100 + (p.sin(p.frameCount / 10) * 50), 100);
-        // for (let i = 0; i < 5; i++) {
-        //     p.ellipse(width / 2 + p.sin(p.frameCount / 20) * 130,
-        //         height / 3 + (i * 50), 40, 40);
-        // }
-        // for (let i = 0; i < 5; i++) {
-        //     p.fill(p.mouseX % 255);
-        //     p.ellipse(
-        //         p.mouseX + (p.sin(p.frameCount / (i + 10)) * (i + 70)),
-        //         p.mouseY - 70 + (i * 50),
-        //         40,
-        //         40);
-        // }
 
-
-        //let speed = p.abs(p.mouseX - p.pmouseX) + p.abs(p.mouseY - p.pmouseY);
-        //p.ellipse(x + p.random(-extra, extra), y, speed, speed);
-        //sin is a better oscillation technique then dividing by frame rate 
-        //for (let i = 0; i < 4; i++) {
-
-        /// like a firefly
-
-        // var lineColor = p.color('white')
-        // lineColor.setAlpha(128 + 128 * p.sin(p.millis() / 1000));
-        // p.fill(lineColor)
-        // for (let i = 0; i < 5; i++) {
-        //     p.ellipse(p.mouseX + p.sin(xpos) * 100, p.mouseY + p.sin(ypos) * 100, 15, 15); /// alt with speed, 15,15 - which is the size of the dot
-        //     //this gives a jitter //p.random(-0.05, 0.05);
-        //     xpos += 0.05 + p.random() * 0.05;
-        // }
-
-        // ypos += 0.04 + p.random() * 0.05; 
-        //firefly
-
-        var lineColor = p.color('white')
-        //lineColor.setAlpha(128 + 128 * p.sin(p.millis() / 1000));
-        //p.fill(lineColor)
         p.stroke('white')
-        for (let i = 0; i < 5; i++) {
+        for (let i = 0; i < 10; i++) {
             p.ellipse(p.mouseX + p.sin(xpos) * p.random(100), p.mouseY + p.sin(ypos) * p.random(100), 15, 15); /// alt with speed, 15,15 - which is the size of the dot
             //this gives a jitter //p.random(-0.05, 0.05);
             xpos += 0.005 // + p.random() * 0.05;
             ypos += 0.004 // + p.random() * 0.05;
         }
 
-        //ypos += 0.04 + p.random() * 0.05; //p.random(-0.04, 0.04);
-        //}
 
 
-        p.translate(p.width / 2, p.height / 2)
+        p.translate(p.width / 2, p.height / 2) // added this so it would draw vertexes from the center of the canvas
         //kaliedescope vertice drawing /// change out p.angleMode(p.DEGREES) and take away background color for a different effect 
         p.beginShape();
         let spacing = p.map(p.mouseX, 0, width / 2, 5, 10)
         //let spacing = p.map(p.mouseX, 0, width, 5, 100) //originally this was this
         console.log(spacing)
-        for (let a = 0; a < 360; a += spacing) {
-            let x = 100 * p.sin(a); //+ 200;
-            let y = 100 * p.cos(a); //+ 200;
-            p.vertex(x, y);
+
+        //setting up a bit of a condition so a line maintains a circle -- but only on the left side of the x 0 axis...
+        if (spacing > 12.34) {
+            spacing = 12.84
+        }
+        //this is to keep the circle drawing form disappearing. but if you DO want to show it being drawn out and not closed, just take this second condition check away. 
+        if (spacing < 6.4) {
+            spacing = 6.4
         }
 
-        // p.vertex(100, 5);
-        // p.vertex(200, 20);
-        // p.vertex(200, 100);
-        // p.vertex(50, 75);
-        // p.vertex(25, 50);
+        for (let a = 0; a < 360; a += spacing) {
+            let x = 200 * p.sin(a);
+            let y = 200 * p.cos(a);
+            p.vertex(x + p.noise(x * noiseScale) * 10, y + p.noise(y * noiseScale) * 10);
+        }
+        console.log(p.noise(spacing * noiseScale) * 10)
 
-        //p.curveVertex(100, 200)
-        // p.curveVertex(p.mouseX, p.mouseY); ///this changes the entry point into the curve
-        // p.curveVertex(100, 200);
-        // p.curveVertex(150, 50);
-        // p.curveVertex(250, 60);
-        // p.curveVertex(300, 200);
-        // p.curveVertex(300, 200);
+        p.endShape();
 
-        // p.curveVertex(100, 200); //repeating entry exit points to complete curve. imperfect because of the wrapper. it's making a hard angle 
-        // p.curveVertex(100, 200);
-        // p.curveVertex(150, 50);
-        // p.curveVertex(250, 60);
-        // p.curveVertex(300, 200);
-        // p.curveVertex(300, 200);
-        p.endShape(); /// removed p.CLOSE
+        noiseScale += 0.0001
     };
 
 
@@ -206,3 +157,48 @@ export default function sketch3(p) {
 
 
 }
+
+
+//frame count can only grow linearly so it's not super helpful for animations
+//p.ellipse(p.frameCount, 200, 100, 50 + (p.frameCount / 2));
+// p.ellipse(p.frameCount, 200, 100, 50 + (p.frameCount % 30) * 10, 50);
+//p.ellipse(width / 2, height / 2, (p.frameCount % 30) * 5, 160);
+//ellipse(200, 200, 100+(sin(frameCount)*50), 100);
+///To make the oscillation slower, we need to divide the frameCount value by some other value. Let’s make it ten times slower by dividing by ten:
+// p.ellipse(width / 2, height / 2, 100 + (p.sin(p.frameCount / 10) * 50), 100);
+// for (let i = 0; i < 5; i++) {
+//     p.ellipse(width / 2 + p.sin(p.frameCount / 20) * 130,
+//         height / 3 + (i * 50), 40, 40);
+// }
+// for (let i = 0; i < 5; i++) {
+//     p.fill(p.mouseX % 255);
+//     p.ellipse(
+//         p.mouseX + (p.sin(p.frameCount / (i + 10)) * (i + 70)),
+//         p.mouseY - 70 + (i * 50),
+//         40,
+//         40);
+// }
+
+
+//let speed = p.abs(p.mouseX - p.pmouseX) + p.abs(p.mouseY - p.pmouseY);
+//p.ellipse(x + p.random(-extra, extra), y, speed, speed);
+//sin is a better oscillation technique then dividing by frame rate 
+//for (let i = 0; i < 4; i++) {
+
+/// like a firefly
+
+// var lineColor = p.color('white')
+// lineColor.setAlpha(128 + 128 * p.sin(p.millis() / 1000));
+// p.fill(lineColor)
+// for (let i = 0; i < 5; i++) {
+//     p.ellipse(p.mouseX + p.sin(xpos) * 100, p.mouseY + p.sin(ypos) * 100, 15, 15); /// alt with speed, 15,15 - which is the size of the dot
+//     //this gives a jitter //p.random(-0.05, 0.05);
+//     xpos += 0.05 + p.random() * 0.05;
+// }
+
+// ypos += 0.04 + p.random() * 0.05; 
+//firefly
+
+// var lineColor = p.color('white')
+// lineColor.setAlpha(128 + 128 * p.sin(p.millis() / 1000));
+// p.fill(lineColor)
