0529031db3f3b868762da3f91e3531c9574e7341
cepholopoddreamz <leslie.plumb@gmail.com>
2020-06-03 02:23:06 +0000

    adding hsb color parameters and incremental shifts to color value over time

--- client/src/components/sketches/sketch7.js
+++ client/src/components/sketches/sketch7.js
@@ -5,14 +5,14 @@ import p5 from "react-p5-wrapper/node_modules/p5";
 export default function sketch7(p) {
   var v;
   var vertices = [];
-  var num = 10,
-    //i changed d to diam ---> which may need to be updated in the 'OLD CODE' section
+  var num = 10; // not currently in use // this is the number of points that you could pass as a value into the calculation for vertex locations, along the path of a circle
+  //i changed d to diam ---> which may need to be updated in the 'OLD CODE' section
 
-    maxDist = 15,
-    minDist = 10;
-  var saveImg = false;
+  //   maxDist = 15,
+  //   minDist = 10;
+  // var saveImg = false;
 
-  var t;
+  var t; //this is a time increment being passed inthe the noise function and progressively being increased over time
 
   let diam = 250
 
@@ -47,13 +47,55 @@ export default function sketch7(p) {
 
   let incrNoise = 0.001;
 
+  let color = {
+    h: 18,
+    s: 80,
+    b: 100 // 0 goes to dark values
+  }
+
+  let colorInr = 0.1
+  let dirH = 1
+  let dirS = 1
+  let dirB = 1
+
   p.draw = function () {
 
-    p.background(238);
+
     //var r = 1;
     // var scaleAmount = p.mouseX / 200;
     // p.scale(scaleAmount);
 
+
+
+    if (color.h >= 15) {
+      dirH = -1
+    }
+    if (color.h <= 9) {
+      dirH = 1
+    }
+    // if (color.s <= 50) {
+    //   dirS = 1
+    // }
+    // if (color.s >= 255) {
+    //   dirS = -1
+    // }
+
+
+    if (color.b >= 80) {
+      dirS = -1
+    }
+    if (color.b <= 25) {
+      dirS = 1
+    }
+
+    color.h += colorInr * dirH
+    //color.s += colorInr * dirS
+    color.b += colorInr * dirB
+
+    p.colorMode(p.HSB, 100);
+    p.background(54, 100, 100);
+
+    console.log(color.s)
     p.translate(p.width / 2, p.height / 2);
     //when i introduced translate i no longer needed to have width/2 + p.sin((p.TWOPI)) etc etc
     //p.scale(p.mouseX / 500, p.mouseY / 500);
@@ -64,6 +106,8 @@ export default function sketch7(p) {
 
     // nums < 3 ? nums = 3 : nums = nums; // this was so the minimum shape that would be drawn was a triangle
     //console.log(nums)
+
+    //if i want to mouse the circle's diamter according to mouse movements:
     diam = p.mouseX
 
     if (p.mouseX > 400) {
@@ -72,6 +116,9 @@ export default function sketch7(p) {
     if (p.mouseX < 50) {
       diam = 50
     }
+
+    //playing with different ways of injecting a mouse dependent data into how the noise function incrementally changes over time
+    //let incr = p.map(p.mouseX, 0, p.width, 0, 0.01)
 
     //12 was originally the value nums
     for (var i = 0; i < 22; i++) {
@@ -82,24 +129,26 @@ export default function sketch7(p) {
 
 
     p.beginShape()
-    p.stroke('purple')
+    p.stroke(color.h, color.s, color.b)
     //p.strokeWeight(5)
-    p.fill('white')
+    p.fill(color.h, color.s, color.b)
     //p.noFill()
     for (var i = 0; i < vertices.length; i++) {
       let x = vertices[i].x
       let y = vertices[i].y
 
 
-      console.log(p.noise(t))
+      //console.log(p.noise(t))
       //p.point(x * p.noise(t + p.random(0.01, 0.05)), y * p.noise(t + p.random(0.01, 0.05)))
       //this allows me to draw a circle, but then manipulate particular vertex points along its path. so i can get the wiggles and the shakes. 
       p.curveVertex(x * p.noise(t + p.random(0.01, 0.05)), y * p.noise(t + p.random(0.01, 0.05)))
+      //p.curveVertex(x * p.noise(t), y * p.noise(t))
+
     }
     p.endShape(p.CLOSE)
 
     t += 0.002;
-
+    //t += incr; //see note above for how incr is being experimented with 
 
     //OLD CODE
 
