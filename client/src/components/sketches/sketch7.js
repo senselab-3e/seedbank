ab7bcb6bbb516a43fa284228aafef11a6f07e248
cepholopoddreamz <leslie.plumb@gmail.com>
2020-06-04 06:14:19 +0000

    cleaning up and adding a few more notes

--- client/src/components/sketches/sketch7.js
+++ client/src/components/sketches/sketch7.js
@@ -142,98 +142,58 @@ export default function sketch7(p) {
     //playing with different ways of injecting a mouse dependent data into how the noise function incrementally changes over time
     //let incr = p.map(p.mouseX, 0, p.width, 0, 0.01)
 
-    //12 was originally the value nums
+
     for (var i = 0; i < num; i++) {
       var x = p.sin((p.TWO_PI / num) * i) * diam;
       var y = p.cos((p.TWO_PI / num) * i) * diam;
       vertices[i] = p.createVector(x, y);
     }
 
-
-    p.beginShape()
-    p.stroke('purple')
     //p.stroke(color.h, color.s, color.b)
-    p.strokeWeight(5)
-    p.fill(color.h, color.s, color.b, 50)
+
+    p.push()
+    p.stroke('white')
+    p.noFill()
+    p.translate(p.width / 2, p.height / 2);
+    p.beginShape()
     //p.noFill()
     for (var i = 0; i < vertices.length; i++) {
       let x = vertices[i].x
       let y = vertices[i].y
-
-
-      //console.log(p.noise(t))
-      //p.point(x * p.noise(t + p.random(0.01, 0.05)), y * p.noise(t + p.random(0.01, 0.05)))
       //this allows me to draw a circle, but then manipulate particular vertex points along its path. so i can get the wiggles and the shakes. 
       p.curveVertex(x * p.noise(t + p.random(0.01, 0.05)), y * p.noise(t + p.random(0.01, 0.05)))
-      //p.curveVertex(x * p.noise(t), y * p.noise(t))
+
       //p.point(x, y)
 
     }
     p.endShape(p.CLOSE)
 
     t += 0.001
-    //p.frameRate % 100 === 0 ? t += 0.001 : t += 0.0001
-    //t += incr; //see note above for how incr is being experimented with 
 
+    p.pop()
 
+    ///NOTES: values being passed into Vector object constructor
 
-    //OLD CODE
-
-    // p.beginShape();
-    // p.fill(p.pmouseX % 255, p.pmouseY % 255, p.mouseY % 255);
-    // p.stroke('yellow');
-    // p.strokeWeight(1);
-    // for (var i = 0; i < vertices.length; i++) {
-    //   vertices[i].x += p.random(-r, r);
-    //   vertices[i].y += p.random(-r, r);
-    //   var next = (i + 1) % num;
-    //   vertices[next].x += p.random(-r, r);
-    //   vertices[next].y += p.random(-r, r);
-    //   //console.log(Vector);
-    //   v = p5.Vector.sub(vertices[next], vertices[i]);
-
-    //   v.normalize();
-    //   v.setMag(maxDist);
-
-    //   vertices[next] = p5.Vector.add(vertices[i], v);
-
-    //   for (var j = 0; j < vertices.length; j++) {
-    //     drawLines()
-    //     if (j !== i) {
-    //       var d = p5.Vector.dist(vertices[i], vertices[j]);
-    //       if (d < minDist) {
-    //         var temp = p5.Vector.sub(vertices[i], vertices[j]);
-    //         temp.normalize();
-    //         temp.setMag(minDist);
-    //         vertices[i] = p5.Vector.add(vertices[j], temp);
-    //       }
-    //     }
-    //   }
-    //   p.vertex(vertices[i].x, vertices[i].y);
-    // }
-    // p.endShape(p.CLOSE);
-
-
-    // OLD CODE ABOVE
-    // if (saveImg && p.frameCount % 2 === 0 && p.counter < 400) {
-    //   p.save("image-" + p.counter + ".png");
-    //   p.counter++;
-    // }
-
-    let width = 500
-    //let height = 500
     let cols = 6;
-    let elSize = 70
+    let elSize = 50
     let elVertex = 25
-    let grids = []
 
+    p.fill(color.h, color.s, color.b, 50)
+
+    //NOTES: values being passed into the grid position calculations, which allows a translate function to be applied to each of those vector locations, in the Vector Object constructor, later
+    let width = p.width // canvas size across which the grid should be calculated and distributed
+    let grids = []
     let mult = width / cols
 
     let vectors = []
 
+
+
     //this calculates my grid --- specifically the centripical center points out of which my vectors for the elements will be drawn
     //it's a way to generate the translate offset values. 
+
     //p.translate(-p.width / 2, -p.height / 2)
+    // i've set the var to start at 1, instead of 0, because i don't want the elements at the far edges of the canvas
     for (var i = 1; i < cols; i++) {
 
       for (var m = 1; m < cols; m++) {
@@ -316,9 +276,7 @@ export default function sketch7(p) {
         //console.log(p.noise(t))
         //p.point(x * p.noise(t + p.random(0.01, 0.05)), y * p.noise(t + p.random(0.01, 0.05)))
         //this allows me to draw a circle, but then manipulate particular vertex points along its path. so i can get the wiggles and the shakes. 
-
         p.curveVertex(xV * p.noise(this.incr + p.random(0.01, 0.09)), yV * p.noise(this.incr + p.random(0.01, 0.09)))
-
         //p.curveVertex(x * p.noise(t), y * p.noise(t))
         //p.point(xV, yV)
 
@@ -337,4 +295,47 @@ export default function sketch7(p) {
 
 // function windowResized() {
 //   resizeCanvas(windowWidth, windowHeight-4);
+// }
+
+//OLD CODE
+
+// p.beginShape();
+// p.fill(p.pmouseX % 255, p.pmouseY % 255, p.mouseY % 255);
+// p.stroke('yellow');
+// p.strokeWeight(1);
+// for (var i = 0; i < vertices.length; i++) {
+//   vertices[i].x += p.random(-r, r);
+//   vertices[i].y += p.random(-r, r);
+//   var next = (i + 1) % num;
+//   vertices[next].x += p.random(-r, r);
+//   vertices[next].y += p.random(-r, r);
+//   //console.log(Vector);
+//   v = p5.Vector.sub(vertices[next], vertices[i]);
+
+//   v.normalize();
+//   v.setMag(maxDist);
+
+//   vertices[next] = p5.Vector.add(vertices[i], v);
+
+//   for (var j = 0; j < vertices.length; j++) {
+//     drawLines()
+//     if (j !== i) {
+//       var d = p5.Vector.dist(vertices[i], vertices[j]);
+//       if (d < minDist) {
+//         var temp = p5.Vector.sub(vertices[i], vertices[j]);
+//         temp.normalize();
+//         temp.setMag(minDist);
+//         vertices[i] = p5.Vector.add(vertices[j], temp);
+//       }
+//     }
+//   }
+//   p.vertex(vertices[i].x, vertices[i].y);
+// }
+// p.endShape(p.CLOSE);
+
+
+// OLD CODE ABOVE
+// if (saveImg && p.frameCount % 2 === 0 && p.counter < 400) {
+//   p.save("image-" + p.counter + ".png");
+//   p.counter++;
 // }
