bc267135cdc35907e893a21090e81a1a2e55b973
cepholopoddreamz <leslie.plumb@gmail.com>
2020-06-05 01:31:04 +0000

    cleaning up the code, removing some notes, but still some scraggly old code at the bottom for notes and references. independent color values now being past to each instance, by moving the constructor into setup, instead of inside the draw function. color shifting is also now occuring with the element constructor object. we have cepholopod skin

--- client/src/components/sketches/sketch7.js
+++ client/src/components/sketches/sketch7.js
@@ -16,34 +16,58 @@ export default function sketch7(p) {
 
   let diam = 250
 
+  let vectors = []
 
-  // function drawLines() {
+  //var cnv //this was for when i needed to assign canvas a value, so that i could pass a function to it.
 
-  //   p.stroke(60, 232, 192)
-  //   //p.stroke('#ffffff')
-  //   // p.noFill()
-  //   p.line(p.mouseX, p.mouseY, p.pmouseX, p.pmouseY);
-  //   // if (p.mouseIsPressed === true) {
-  //   //   // p.line(p.mouseX, 55, p.mouseX, p.pmouseY);
-
-  //   // } else {
-  //   //   p.line(p.mouseX, p.mouseY, p.pmouseX, p.pmouseY);
-  //   // }
-
-  // }
-  var cnv
+  let colorInr = 0.05
+  let dirH = 1
+  let dirS = 1
+  let dirB = 1
 
   p.setup = function () {
-    cnv = p.createCanvas(500, 500);
+    //cnv = p.createCanvas(500, 500); **see note above
+    p.createCanvas(500, 500);
     t = 0;
     p.frameRate = 10
-    // let nums = p.map(p.mouseX, 0, p.width, 20, 60)
 
-    // for (var i = 0; i < num; i++) {
-    //   var x = p.width / 2 + p.sin((p.TWO_PI / num) * i) * diam;
-    //   var y = p.height / 2 + p.cos((p.TWO_PI / num) * i) * diam;
-    //   vertices[i] = p.createVector(x, y);
-    // }
+    ///NOTES: values being passed into Vector object constructor
+    let cols = 16;
+    let elSize = 10
+    let elVertex = 25
+
+    p.fill(color.h, color.s, color.b, 50)
+
+    //NOTES: values being passed into the grid position calculations, which allows a translate function to be applied to each of those vector locations, in the Vector Object constructor, later
+    let width = p.width // canvas size across which the grid should be calculated and distributed
+    let grids = []
+    let mult = width / cols
+    //this calculates my grid --- specifically the centripical center points out of which my vectors for the elements will be drawn
+    //it's a way to generate the translate offset values. 
+
+    //p.translate(-p.width / 2, -p.height / 2)
+    // i've set the var to start at 1, instead of 0, because i don't want the elements at the far edges of the canvas
+    for (let i = 1; i < cols; i++) {
+
+      for (var m = 1; m < cols; m++) {
+        grids.push(p.createVector(i * mult, m * mult))
+      }
+    }
+    // p.stroke('white')
+    // p.noFill()
+    let colorArray = []
+
+    for (var a = 0; a < grids.length; a++) {
+      let x = grids[a].x
+      let y = grids[a].y
+      let incrOrg = 0.001
+      let incr = p.random(0.002, 0.009) // amount to be passed to noise values in the constructor - in an attempt to not have every instance be moving in exactly the same way. 
+      let ranHueVal = p.floor(p.random(100)) // doesn't work. runs continuously
+
+      colorArray.push(ranHueVal) // doesn't work. runs continuously
+      let el = new Vector(x, y, elSize, elVertex, incrOrg, incr, colorArray[a])
+      vectors.push(el)
+    }
   };
 
 
@@ -54,106 +78,26 @@ export default function sketch7(p) {
     b: 100 // 0 goes to dark values
   }
 
-  let colorInr = 0.1
-  let dirH = 1
-  let dirS = 1
-  let dirB = 1
 
-  p.updateColor = function () {
-    //yellow spectrum
-    // if (color.h >= 15) {
-    //   dirH = -1
-    // }
-    // if (color.h <= 9) {
-    //   dirH = 1
-    // }
-    //fullspectrum
-    //console.log(p.mouseX, p.pmouseX) // the previous mouse value is always printing the same as mouseX.. so always comes to a null
 
-    //if (p.mouseX > p.pmouseX) {
-    if (color.h >= 100) {
-      dirH = -1
-    }
-    // } else if (p.mouseX < p.pmouseX) {
-    if (color.h <= 0) {
-      dirH = 1
-    }
-    //}
 
-    if (color.s >= 100) {
-      dirS = -1
-    }
-    if (color.s <= 30) {
-      dirS = 1
-    }
 
-    color.h += colorInr * dirH
-    color.s += colorInr * dirS
-    color.b += colorInr * dirB
-  }
 
   p.draw = function () {
-
-
-
-
-
-    //if (p.mouseY !== p.pmouseY) {
-    // if (color.b >= 100) {
-    //   dirB = -1
-    // }
-    // if (color.b <= 80) {
-    //   dirB = 1
-    // }
-    //}
-
-    //var r = 1;
-    // var scaleAmount = p.mouseX / 200;
-    // p.scale(scaleAmount);
-
-
-    cnv.mouseMoved(p.updateColor) // only on mouse move, trigger shifts in color values
-
-
     p.colorMode(p.HSB, 100);
-    //p.background(54, 100, 100);
     p.background(67)
 
-    //console.log(p.floor(color.h), dirH)
-    //p.translate(p.width / 2, p.height / 2);
-    //when i introduced translate i no longer needed to have width/2 + p.sin((p.TWOPI)) etc etc
-    //p.scale(p.mouseX / 500, p.mouseY / 500);
-
-    //this is useful if i want the number of vertexes to correspond with the mouse movements. 
-    // let nums = p.map(p.mouseY, 0, p.height, 0, 30)
-    // nums = p.floor(nums)
-
-    // nums < 3 ? nums = 3 : nums = nums; // this was so the minimum shape that would be drawn was a triangle
-    //console.log(nums)
-
-    //NOTE--->if i want to mouse the circle's diamter according to mouse movements:
-    // diam = p.mouseX
-
-    // if (p.mouseX > 400) {
-    //   diam = 400
-    // }
-    // if (p.mouseX < 50) {
-    //   diam = 50
-    // }
-
-    //playing with different ways of injecting a mouse dependent data into how the noise function incrementally changes over time
-    //let incr = p.map(p.mouseX, 0, p.width, 0, 0.01)
-
-
-    for (let i = 0; i < num; i++) {
-      var x = p.sin((p.TWO_PI / num) * i) * diam;
-      var y = p.cos((p.TWO_PI / num) * i) * diam;
-      vertices[i] = p.createVector(x, y);
+    for (let n = 0; n < vectors.length; n++) {
+      vectors[n].show() // this draws out the circles as intended. 
+      //svectors[n].squares() // this is a quick test of a constructor squares instead of circles
+      vectors[n].colors()
     }
-
-    //p.stroke(color.h, color.s, color.b)
-
     //central vibrating circle
+    // for (let i = 0; i < num; i++) {
+    //   var x = p.sin((p.TWO_PI / num) * i) * diam;
+    //   var y = p.cos((p.TWO_PI / num) * i) * diam;
+    //   vertices[i] = p.createVector(x, y);
+    // }
 
     // p.push()
     // p.stroke('white')
@@ -177,51 +121,6 @@ export default function sketch7(p) {
 
     // p.pop()
 
-    ///NOTES: values being passed into Vector object constructor
-
-    let cols = 6;
-    let elSize = 90
-    let elVertex = 25
-
-    p.fill(color.h, color.s, color.b, 50)
-
-    //NOTES: values being passed into the grid position calculations, which allows a translate function to be applied to each of those vector locations, in the Vector Object constructor, later
-    let width = p.width // canvas size across which the grid should be calculated and distributed
-    let grids = []
-    let mult = width / cols
-
-    let vectors = []
-
-
-
-    //this calculates my grid --- specifically the centripical center points out of which my vectors for the elements will be drawn
-    //it's a way to generate the translate offset values. 
-
-    //p.translate(-p.width / 2, -p.height / 2)
-    // i've set the var to start at 1, instead of 0, because i don't want the elements at the far edges of the canvas
-    for (let i = 1; i < cols; i++) {
-
-      for (var m = 1; m < cols; m++) {
-        grids.push(p.createVector(i * mult, m * mult))
-      }
-    }
-    // p.stroke('white')
-    // p.noFill()
-    for (var a = 0; a < grids.length; a++) {
-      let x = grids[a].x
-      let y = grids[a].y
-      let incrOrg = 0.001
-      let incr = p.random(0.002, 0.009) // amount to be passed to noise values in the constructor - in an attempt to not have every instance be moving in exactly the same way. 
-      //p.point(x, y)
-      //p.ellipse(x, y, 50, 50)
-      let el = new Vector(x, y, elSize, elVertex, incrOrg, incr, color)
-      vectors.push(el)
-    }
-
-    for (let n = 0; n < vectors.length; n++) {
-      //vectors[n].show() // this draws out the circles as intended. 
-      vectors[n].squares() // this is a quick test of a constructor squares instead of circles
-    }
   };
 
 
@@ -246,7 +145,7 @@ export default function sketch7(p) {
 
 
   class Vector {
-    constructor(x, y, diam, vertex, incrOrg, incr, color) {
+    constructor(x, y, diam, vertex, incrOrg, incr, colRan) {
       this.x = x;
       this.y = y;
       this.cirVertices = vertex
@@ -254,21 +153,19 @@ export default function sketch7(p) {
       this.elements = []
       this.incrOrg = incrOrg
       this.incr = incr //p.random(0.002, 0.006) //incr //p.random(0.002, 0.006)
-      this.color = color // this isn't currently being used --- as the univeral faill color and its shifts is going to the elements// but i could pull this out later. 
+      this.colorObj = {
+        h: colRan, //p.random(255), // random from here, also runs continuously
+        s: colRan,
+        b: 100,
+        a: colRan
 
+      }
     }
 
     show() {
-      //let xAround = p.sin((p.TWO_PI / num) * i) * this.diam;
-      //p.ellipse(this.x, this.y, this.diam, this.diam)
-      console.log(this.incr, 'apples')
       this.incrOrg += this.incr
-      // p.push()
-      // p.translate(this.x, this.y)
-
-      // p.ellipse(0, 0, this.diam, this.diam)
-      // p.pop()
-      p.push()
+      p.push() ///this push is super important
+      p.fill(this.colorObj.h, this.colorObj.s, this.colorObj.b, this.colorObj.a)
       p.translate(this.x, this.y)
       p.noStroke()
       for (let g = 0; g < this.cirVertices; g++) {
@@ -281,30 +178,50 @@ export default function sketch7(p) {
       for (let v = 0; v < this.elements.length; v++) {
         let xV = this.elements[v].x
         let yV = this.elements[v].y
-
-
-        //console.log(p.noise(t))
-        //p.point(x * p.noise(t + p.random(0.01, 0.05)), y * p.noise(t + p.random(0.01, 0.05)))
         //this allows me to draw a circle, but then manipulate particular vertex points along its path. so i can get the wiggles and the shakes. 
         p.curveVertex(xV * p.noise(this.incrOrg + p.random(0.01, 0.09)), yV * p.noise(this.incrOrg + p.random(0.01, 0.09)))
-        //p.curveVertex(x * p.noise(t), y * p.noise(t))
-        //p.point(xV, yV)
-
       }
       p.endShape(p.CLOSE)
       p.pop()
-
       //this.incr += this.incr
 
     }
 
     colors() {
 
+      //this keeps the above random for color values from running repeatedly...BUT it's being applied to all the randoms, whehn i only want it to run on the color object
+      //p.colorPhase(this.colorObj)
+      // console.log(this.colorObj.h)
+      p.push()
+      if (this.colorObj.h >= 100) {
+        dirH = -1
+      }
+      // } else if (p.mouseX < p.pmouseX) {
+      if (this.colorObj.h <= 0) {
+        dirH = 1
+      }
+
+      if (this.colorObj.s >= 100) {
+        dirS = -1
+      }
+      // } else if (p.mouseX < p.pmouseX) {
+      if (this.colorObj.s <= 50) {
+        dirS = 1
+      }
+
+      this.colorObj.h += colorInr * dirH
+      this.colorObj.s += colorInr * dirS
+      //this.colorObj.b += colorInr * dirB
+
+      // console.log(this.colorObj.h)
+      p.pop()
     }
 
     squares() {
       this.incrOrg += this.incr
       p.noStroke()
+      p.push()
+      p.fill(this.colorObj.h, this.colorObj.s, this.colorObj.b)
       p.beginShape()
       p.vertex(this.x - this.diam * p.noise(this.incrOrg + p.random(1.01, 1.09)), this.y - this.diam * p.noise(this.incrOrg + p.random(1.01, 1.05)))
       p.vertex(this.x + this.diam * p.noise(this.incrOrg + p.random(1.01, 1.09)), this.y - this.diam * p.noise(this.incrOrg + p.random(1.01, 1.05)))
@@ -312,9 +229,8 @@ export default function sketch7(p) {
       p.vertex(this.x + this.diam * p.noise(this.incrOrg + p.random(1.01, 1.09)), this.y + this.diam * p.noise(this.incrOrg + p.random(1.01, 1.05)))
       p.vertex(this.x - this.diam * p.noise(this.incrOrg + p.random(1.01, 1.09)), this.y + this.diam * p.noise(this.incrOrg + p.random(1.01, 1.05)))
       p.endShape(p.CLOSE)
-
+      p.pop()
     }
-
   }
 }
 
@@ -363,4 +279,93 @@ export default function sketch7(p) {
 // if (saveImg && p.frameCount % 2 === 0 && p.counter < 400) {
 //   p.save("image-" + p.counter + ".png");
 //   p.counter++;
+// }
+
+//OLD CODE
+
+
+//p.translate(p.width / 2, p.height / 2);
+//when i introduced translate i no longer needed to have width/2 + p.sin((p.TWOPI)) etc etc
+//p.scale(p.mouseX / 500, p.mouseY / 500);
+
+//this is useful if i want the number of vertexes to correspond with the mouse movements. 
+// let nums = p.map(p.mouseY, 0, p.height, 0, 30)
+// nums = p.floor(nums)
+
+// nums < 3 ? nums = 3 : nums = nums; // this was so the minimum shape that would be drawn was a triangle
+//console.log(nums)
+
+//NOTE--->if i want to mouse the circle's diamter according to mouse movements:
+// diam = p.mouseX
+
+// if (p.mouseX > 400) {
+//   diam = 400
+// }
+// if (p.mouseX < 50) {
+//   diam = 50
+// }
+
+//playing with different ways of injecting a mouse dependent data into how the noise function incrementally changes over time
+//let incr = p.map(p.mouseX, 0, p.width, 0, 0.01)
+
+///OLD METHOD OF DRAWING OBJECTS IN A GRID THAT DOESN't use a constructor.
+
+
+
+// ///NOTES: values being passed into Vector object constructor
+
+// let cols = 6;
+// let elSize = 90
+// let elVertex = 25
+
+// p.fill(color.h, color.s, color.b, 50)
+// //NOTES: values being passed into the grid position calculations, which allows a translate function to be applied to each of those vector locations, in the Vector Object constructor, later
+// let width = p.width // canvas size across which the grid should be calculated and distributed
+// let grids = []
+// let mult = width / cols
+
+// let vectors = []
+
+
+
+// //this calculates my grid --- specifically the centripical center points out of which my vectors for the elements will be drawn
+// //it's a way to generate the translate offset values. 
+
+// //p.translate(-p.width / 2, -p.height / 2)
+// // i've set the var to start at 1, instead of 0, because i don't want the elements at the far edges of the canvas
+// for (let i = 1; i < cols; i++) {
+
+//   for (var m = 1; m < cols; m++) {
+//     grids.push(p.createVector(i * mult, m * mult))
+//   }
+// }
+// // p.stroke('white')
+// // p.noFill()
+// let colorArray = []
+
+// for (var a = 0; a < grids.length; a++) {
+//   let x = grids[a].x
+//   let y = grids[a].y
+//   let incrOrg = 0.001
+//   let incr = p.random(0.002, 0.009) // amount to be passed to noise values in the constructor - in an attempt to not have every instance be moving in exactly the same way. 
+//   let ran = p.floor(p.random(100)) // doesn't work. runs continuously
+//   colorArray.push(ran) // doesn't work. runs continuously
+//   console.log(ran.length)
+
+//   //p.point(x, y)
+//   //p.ellipse(x, y, 50, 50)
+
+//   // p.push()
+//   // //p.randomSeed(1)
+//   // let colorObj = {
+//   //   h: p.random(255),
+//   //   s: 100,
+//   //   b: 100
+//   // }
+//   // p.pop()
+
+//   // p.randomSeed(grids.length)
+
+//   let el = new Vector(x, y, elSize, elVertex, incrOrg, incr, colorArray[a])
+//   vectors.push(el)
 // }
