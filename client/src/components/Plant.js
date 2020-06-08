153644b23b44568ef0a66ebf59dafa081ec1a6f2
essitam <matisseam@hotmail.com>
2020-06-08 17:46:54 -0400

    plant

--- client/src/components/Plant.js
+++ client/src/components/Plant.js
@@ -6,14 +6,15 @@ import glitch from "../assets/img/404_glitch2.gif";
 class Plant extends Component {
 
     render() {
-      var names = [glitch, glitch, glitch];
+      var names = [glitch, glitch, glitch, glitch, glitch, glitch, glitch];
       var namesList = names.map(function(name){
                        return ( <div className="img">
-                            <div className="img__content">
-                               <img src={name}/>
-                           </div>
-                         </div>);
+                                  <div className="img__content">
+                                    <img key={name.toString()} src={name} alt="glitch"/>
+                                  </div>
+                                </div>);
                      })
+                     
      return (
        <div className="divbody">
         <div className="main">
