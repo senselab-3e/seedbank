cc4578d5a42aa078d6a067118f0e1663bc53b312
essitam <matisseam@hotmail.com>
2020-06-22 13:27:48 -0400

    1 of 20,000 sponges

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import About from "./pages/About";
 import Header from "./partials/Header";
 import Glitch from "./pages/Glitch";
 import ImagesPage from "./pages/Images";
+import Sponges from "./components/sponges"
 
 function Home() {
   return (
@@ -26,6 +27,7 @@ class App extends Component {
           <Route exact path="/" component={Home} />
           <Route path="/about3e" component={About} />
           <Route path="/oOoOs" component={Glitch} />
+          <Route path="/sponges" component={Sponges} />
           <Route path="/patches" render={() => <div>Patches</div>} />
           <Route path="/entryway" render={() => <div>Entry</div>} />
           <Route exact path="/auth" component={AuthPage} />
