c53c160e775ab370e542278bb4f8ef80ed67c4ae
cepholopoddreamz <leslie.plumb@gmail.com>
2020-09-25 22:14:42 +0000

    adding back basic prop system to update event list view when a new event is added. im piggy backing in the eventlist page to differentiate the slidertext api events with the the eventpage queries. the same warning comes up in the console when i go to post a new event. ... strange.. but. if i refresh the page, that new event does end up displaying. so.... just continuing to isolate the issue betwen a axios queries that are working as intended vs one that is not

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,5 @@
 import React, { Component } from "react";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import withAuth from "./helpers/withAuth";
 import EventsPage from "./pages/events";
 import AuthPage from "./pages/auth";
@@ -11,7 +7,6 @@ import About from "./pages/About";
 import Header from "./partials/Header";
 import Glitch from "./pages/Glitch";
 import ImagesPage from "./pages/Images";
-
 
 function Home() {
   return (
@@ -21,25 +16,22 @@ function Home() {
   );
 }
 
-
 class App extends Component {
   render() {
     return (
-      
-        <Router> 
-          <Header />
-          <Switch>
-            <Route exact path="/" component={Home}/>
-            <Route path="/about3e" component={About}/>
-            <Route path="/oOoOs" component={Glitch}/>
-            <Route path="/patches" render={() => <div>Patches</div>}/>
-            <Route path="/entryway"render={() => <div>Entry</div>}/>
-            <Route exact path="/auth" component={AuthPage} />
-            <Route exact path="/events" component={withAuth(EventsPage)} />
-            <Route exact path="/traces" component={withAuth(ImagesPage)} />
-          </Switch>
-        </Router>
-      
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about3e" component={About} />
+          <Route path="/oOoOs" component={Glitch} />
+          <Route path="/patches" render={() => <div>Patches</div>} />
+          <Route path="/entryway" render={() => <div>Entry</div>} />
+          <Route exact path="/auth" component={AuthPage} />
+          <Route exact path="/events" component={EventsPage} />
+          <Route exact path="/traces" component={withAuth(ImagesPage)} />
+        </Switch>
+      </Router>
     );
   }
 }
--- client/src/pages/events.js
+++ client/src/pages/events.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import EventCreate from '../components/EventCreate';
-import EventList from '../components/EventList';
+import React from "react";
+import { Link } from "react-router-dom";
+import EventCreate from "../components/EventCreate";
+import EventList from "../components/EventList";
 
 const EventsPage = () => {
-	return (
-		<div>
-			<EventCreate/>
-			<EventList/>
-			<br/><br/>
-			<Link to='/'>Back to entryway</Link>
-		</div>
-	);
-}
+  const updateEventList = () => {
+    console.log("new event added");
+  };
+
+  return (
+    <div>
+      <EventCreate newEvent={updateEventList} />
+      <EventList />
+      <br />
+      <br />
+      <Link to="/">Back to entryway</Link>
+    </div>
+  );
+};
 
 export default EventsPage;
