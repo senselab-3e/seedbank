23acc3b541e8d2c8112bcb5520d178fbc3810bb0
cepholopoddreamz <leslie.plumb@gmail.com>
2020-09-25 00:45:25 +0000

    tried renaming the name of the table to be created in the create texts table mingration file so that its not requesting a table to be created with the same name of one already existing on the remote backend server but still the same error persists. double checked the server.js file is the exact same was what is currently on master but no change in error. no major change in events.js file

--- anarchive/routes/api/events.js
+++ anarchive/routes/api/events.js
@@ -4,50 +4,51 @@ const knex = require("../../db/knex.js");
 
 // GET api/events
 router.get("/", (req, res) => {
-	knex("events")
-		.then(events => {
-			res.send(events);
-		})
-		.catch(err => {
-			console.log(err);
-		});
+  knex("events")
+    .then((events) => {
+      res.send(events);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 // POST api/events
-router.post('/', (req, res) => {
-	knex('events')
-		.insert({
-			name: req.body.name,
-			data: '{sponges: ' + req.body.sponges.toString() + '}'
-		})
-		.then(id => {
-			res.send('Created event')
-		})
-		.catch(err => {
-			console.log(err);
-		})
+router.post("/", (req, res) => {
+  knex("events")
+    .insert({
+      name: req.body.name,
+      data: "{sponges: " + req.body.sponges.toString() + "}",
+    })
+    .then((id) => {
+      res.send("Created event");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 //DELETE api/events
 
 router.delete("/:id", (req, res) => {
-
-	let select = parseInt(req.params.id);
-	console.log('Deleting Id: ' + select)
-	knex("events")
-		.where({
-			id: select
-		})
-		.del()
-		.then(res.send({
-			id: select
-		}))
-		.catch(err => {
-			res.sendStatus(500).json({
-				status: 'Error :(',
-				error: err
-			})
-		});
+  let select = parseInt(req.params.id);
+  console.log("Deleting Id: " + select);
+  knex("events")
+    .where({
+      id: select,
+    })
+    .del()
+    .then(
+      res.send({
+        id: select,
+      })
+    )
+    .catch((err) => {
+      res.sendStatus(500).json({
+        status: "Error :(",
+        error: err,
+      });
+    });
 });
 
 module.exports = router;
--- anarchive/server.js
+++ anarchive/server.js
@@ -1,85 +1,83 @@
-'use strict';
+"use strict";
 
-require('dotenv').config({
-	path: '../.env'
-});
-const express = require('express');
-const session = require('express-session');
-const mysql = require('mysql');
-const bodyParser = require('body-parser');
-const fs = require('fs');
-const path = require('path');
-const socket = require('socket.io');
+require("dotenv").config({ path: "../.env" });
+const express = require("express");
+const session = require("express-session");
+const mysql = require("mysql");
+const bodyParser = require("body-parser");
+const fs = require("fs");
+const path = require("path");
+const socket = require("socket.io");
 
 const node_env = process.env.NODE_ENV;
 const app = express();
 const router = express.Router();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-	extended: false
-}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-if (node_env != 'production') {
-	const morgan = require('morgan');
-	app.use(morgan('combined'))
-};
+if (node_env != "production") {
+  const morgan = require("morgan");
+  app.use(morgan("combined"));
+}
 
 // Configure API routes
 var routes_path = "./routes/api/";
-var api_routes = fs.readdirSync(routes_path).filter(r => r != '.DS_Store');
+var api_routes = fs.readdirSync(routes_path).filter((r) => r != ".DS_Store");
 for (var i = 0; i < api_routes.length; i++) {
-	var route = api_routes[i].slice(0, -3);
-	app.use("/api/" + route, require(routes_path + route));
-};
+  var route = api_routes[i].slice(0, -3);
+  app.use("/api/" + route, require(routes_path + route));
+}
 
-if (['production', 'local-bundled'].includes(node_env)) {
-	app.use(express.static('../client/build'));
-	app.get('*', function (req, res) {
-		res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
-	});
-};
+if (["production", "local-bundled"].includes(node_env)) {
+  app.use(express.static("../client/build"));
+  app.get("*", function (req, res) {
+    res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
+  });
+}
 
 function start_connection() {
-	const db = mysql.createConnection({
-		host: process.env.HOST,
-		database: process.env.DB_NAME,
-		user: process.env.DB_USER,
-		password: process.env.DB_PASSWORD
-	});
+  const db = mysql.createConnection({
+    host: process.env.HOST,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+  });
 
-	db.connect(function (err) {
-		if (err) throw err;
-		console.log('Connected to database');
-	})
+  db.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected to database");
+  });
 
-	db.on('error', function (err) {
-		if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-			console.log('Connection lost; restarting');
-			start_connection();
-		} else {
-			throw err;
-		}
-	})
+  db.on("error", function (err) {
+    if (err.code === "PROTOCOL_CONNECTION_LOST") {
+      console.log("Connection lost; restarting");
+      start_connection();
+    } else {
+      throw err;
+    }
+  });
 }
 
 start_connection();
 
-//console.log('Running in ' + node_env + ' mode');
+console.log("Running in " + node_env + " mode");
 
-var server = app.listen(process.env.HTTP_PORT, () => console.log(`Listening on port ${process.env.HTTP_PORT}`));
+var server = app.listen(process.env.HTTP_PORT, () =>
+  console.log(`Listening on port ${process.env.HTTP_PORT}`)
+);
 
 var io = socket(server);
 
-io.sockets.on('connection', newConnection);
+io.sockets.on("connection", newConnection);
 
 function newConnection(socket) {
-	console.log("newconnection " + socket.id);
+  console.log("newconnection " + socket.id);
 
-	socket.on('mouse', mouseMsg);
+  socket.on("mouse", mouseMsg);
 
-	function mouseMsg(data) {
-		socket.broadcast.emit('mouse', data);
-		console.log(data);
-	}
+  function mouseMsg(data) {
+    socket.broadcast.emit("mouse", data);
+    console.log(data);
+  }
 }
