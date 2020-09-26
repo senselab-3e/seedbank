b4b10c5cca5d057cb887864a63d061d6762246a3
cepholopoddreamz <leslie.plumb@gmail.com>
2020-09-26 02:41:41 +0000

    modest refactoring for passing around event list updates. ill refactor further using hooks but for now using the classic props and this state methods. all of this is really just to verify the axios post and get methods for troubleshooting the sliderTexts

--- client/src/components/EventList.js
+++ client/src/components/EventList.js
@@ -1,34 +1,16 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React from "react";
 
-class EventList extends Component {
-
-	constructor() {
-		super();
-		this.state = {
-			events: []
-		}
-	}
-	componentDidMount() {
-		axios.get('/api/events')
-			 .then(events => {
-			 	this.setState({events: events.data})
-			 })
-			 .catch(err => console.log(err))
-	}
-	render() {
-		var events = this.state.events;
-		return (
-			<div>
-				<p>Events:</p>
-				<ul>
-					{ events.map(({ id, name, data }) => (
-						<li key={id.toString()}>{name} {data}</li>
-					))}
-				</ul>
-			</div>
-		)
-	}
+export default function EventList(props) {
+  const events = props.events;
+  return (
+    <div>
+      <ul>
+        {events.map(({ id, name, data }) => (
+          <li key={id.toString()}>
+            {name} {data}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
-
-export default EventList;
--- client/src/pages/events.js
+++ client/src/pages/events.js
@@ -1,22 +1,56 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
+
 import { Link } from "react-router-dom";
 import EventCreate from "../components/EventCreate";
 import EventList from "../components/EventList";
 
-const EventsPage = () => {
-  const updateEventList = () => {
+//   return (
+//     <div>
+//       <EventCreate newEvent={updateEventList} />
+//       <EventList />
+//       <br />
+//       <br />
+//       <Link to="/">Back to entryway</Link>
+//     </div>
+//   );
+// };
+
+// export default EventsPage;
+
+export class events extends Component {
+  constructor() {
+    super();
+    this.state = {
+      events: [],
+    };
+  }
+
+  updateEventList = () => {
     console.log("new event added");
+    axios
+      .get("/api/events")
+      .then((events) => {
+        this.setState({ events: events.data });
+      })
+      .catch((err) => console.log(err));
   };
 
-  return (
-    <div>
-      <EventCreate newEvent={updateEventList} />
-      <EventList />
-      <br />
-      <br />
-      <Link to="/">Back to entryway</Link>
-    </div>
-  );
-};
+  componentDidMount() {
+    this.updateEventList();
+  }
 
-export default EventsPage;
+  render() {
+    return (
+      <div>
+        <EventCreate newEvent={this.updateEventList} />
+        <EventList events={this.state.events} />
+        <br />
+        <br />
+        <Link to="/">Back to entryway</Link>
+      </div>
+    );
+  }
+}
+
+export default events;
