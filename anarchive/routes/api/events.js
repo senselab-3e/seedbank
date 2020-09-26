817defb98b5615684d5de079292b05b7e1660410
cepholopoddreamz <leslie.plumb@gmail.com>
2020-09-26 02:19:55 +0000

    ok many of the issues i had with events posting was because of the way i was runing the anarchive and client concurrently, as well as a client that was running on nodemon. ive written documentation on this so as to understand things more fully. while this is happening i get an econnreset warning but thats ok. i need to run things this way so i can have the client run with nodemon

--- anarchive/routes/api/events.js
+++ anarchive/routes/api/events.js
@@ -1,20 +1,29 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const knex = require('../../db/knex.js');
+const knex = require("../../db/knex.js");
 
 // GET api/events
-router.get('/', (req, res) => {
-	knex('events')
-		.then(events => { res.send(events); })
-		.catch(err => { console.log(err); })
+router.get("/", (req, res) => {
+	knex("events")
+		.then((events) => {
+			res.send(events);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 });
 
 // POST api/events
-router.post('/', (req, res) => {
-	knex('events')
-		.insert({ name: req.body.name,
-				  data: '{sponges: ' + req.body.sponges.toString() + '}' })
-		.catch(err => { console.log(err); })
+router.post("/", (req, res) => {
+	knex("events")
+		.insert({
+			name: req.body.name,
+			data: "{sponges: " + req.body.sponges.toString() + "}",
+		})
+
+		.catch((err) => {
+			console.log(err);
+		});
 });
 
 module.exports = router;
