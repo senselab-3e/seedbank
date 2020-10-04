65b5a89d2da7c74322271424a88003443edb095d
cepholopoddreamz <leslie.plumb@gmail.com>
2020-10-04 04:33:36 +0000

    added working endpoints and api scripts for updating color_patch column of user table in the data base, via the auth api via the CoporPicker component

--- anarchive/routes/api/auth.js
+++ anarchive/routes/api/auth.js
@@ -9,82 +9,134 @@ const Auth = require("../../services/auth-helpers");
 
 // GET /api/auth/verify
 router.get("/verify", (req, res) => {
-  const token = req.headers.authorization.split(" ")[1];
-  if (!token) {
-    res.status(401).json({ status: "No token provided." });
-  } else {
-    jwt.verify(token, process.env.JWT_SECRET, function (err, decoded) {
-      if (err) {
-        res.status(401).json({ status: "Invalid token" });
-      } else {
-        res.status(200).json({ status: "Token good!" });
-        //you can not mess with this response // the status 200 is use in the WithAuth component and only if 200 status is received with whatever page that component wraps, reroute to 'protect' pages that require login information to access.
-      }
-    });
-  }
+    const token = req.headers.authorization.split(" ")[1];
+    if (!token) {
+        res.status(401).json({
+            status: "No token provided."
+        });
+    } else {
+        jwt.verify(token, process.env.JWT_SECRET, function (err, decoded) {
+            if (err) {
+                res.status(401).json({
+                    status: "Invalid token"
+                });
+            } else {
+                res.status(200).json({
+                    status: "Token good!"
+                });
+                //you can not mess with this response // the status 200 is use in the WithAuth component and only if 200 status is received with whatever page that component wraps, reroute to 'protect' pages that require login information to access.
+            }
+        });
+    }
 });
 
 // POST /api/auth/signup
 router.post("/signup", (req, res) => {
-  knex("users")
-    .where({ email: req.body.email })
-    .first()
-    .then(function (user) {
-      if (user) {
-        res.status(401).json({ status: "User already exists!" });
-      } else {
-        bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-          var newUser = {
-            email: req.body.email,
-            password: hash,
-            username: req.body.username,
-          };
+    knex("users")
+        .where({
+            email: req.body.email
+        })
+        .first()
+        .then(function (user) {
+            if (user) {
+                res.status(401).json({
+                    status: "User already exists!"
+                });
+            } else {
+                bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+                    var newUser = {
+                        email: req.body.email,
+                        password: hash,
+                        username: req.body.username,
+                    };
 
-          knex("users")
-            .insert(newUser)
-            .then((newUser) => {
-              return Auth.createToken(newUser);
-            })
-            .then((token) => {
-              res.status(200).json({ status: "Created user!", token: token });
+                    knex("users")
+                        .insert(newUser)
+                        .then((newUser) => {
+                            return Auth.createToken(newUser);
+                        })
+                        .then((token) => {
+                            res.status(200).json({
+                                status: "Created user!",
+                                token: token
+                            });
+                        });
+                });
+            }
+        })
+        .catch((err) => {
+            res.status(500).json({
+                status: "Error :(",
+                error: err
             });
         });
-      }
-    })
-    .catch((err) => {
-      res.status(500).json({ status: "Error :(", error: err });
-    });
 });
 
 // POST /api/auth/login
 router.post("/login", (req, res) => {
-  knex("users")
-    .where({ email: req.body.email })
-    .first()
-    .then(function (user) {
-      if (!user) {
-        res.status(401).json({ status: "User not found :/" });
-      } else {
-        bcrypt.compare(req.body.password, user.password, function (
-          err,
-          result
-        ) {
-          if (result == true) {
-            res
-              .status(200)
-              .json({
-                status: "Successfully logged in!",
-                token: Auth.createToken(user),
-              });
-          } else {
-            res.status(401).json({ status: "Wrong password :(" });
-          }
+    knex("users")
+        .where({
+            email: req.body.email
+        })
+        .first()
+        .then(function (user) {
+            if (!user) {
+                res.status(401).json({
+                    status: "User not found :/"
+                });
+            } else {
+                bcrypt.compare(req.body.password, user.password, function (
+                    err,
+                    result
+                ) {
+                    if (result == true) {
+                        res
+                            .status(200)
+                            .json({
+                                status: "Successfully logged in!",
+                                token: Auth.createToken(user),
+                            });
+                    } else {
+                        res.status(401).json({
+                            status: "Wrong password :("
+                        });
+                    }
+                });
+            }
+        })
+        .catch((err) => {
+            res.status(500).json({
+                status: "Error :(",
+                error: err
+            });
         });
-      }
-    })
-    .catch((err) => {
-      res.status(500).json({ status: "Error :(", error: err });
-    });
+});
+
+
+//PUT /api/auth/:userId
+//THIS is for adding a specific color patch association according to userID ///this is for the eventual pinkest pink integration
+
+router.put("/:userId", (req, res) => {
+    console.log(req.body)
+    let select = parseInt(req.params.userId);
+    knex("users")
+        .where({
+            id: select
+        })
+        // .first()
+        .update({
+            patch_color: req.body.colorPick,
+        })
+        .then(() => {
+            res.send("added color patch");
+        })
+
+        .catch((err) => {
+            res.status(500).json({
+                status: "Error :(",
+                error: err
+            });
+        });
 });
 
 module.exports = router;
--- client/src/components/ColorPicker.js
+++ client/src/components/ColorPicker.js
@@ -1,9 +1,30 @@
 import React, { useState } from "react";
+import { UserId } from "./GetUser";
+import axios from "axios";
 
 export default function ColorPicker(props) {
   const [colorPick, setColorPick] = useState("#ffffff");
 
   console.log(colorPick);
+
+  const submit = (e) => {
+    //weirdly, the state for userId is staying at null. this is nnot a long term fix but for now
+    const userId = UserId();
+    console.log(colorPick, userId, "clicked");
+    e.preventDefault();
+    axios
+      .put(`/api/auth/${userId}`, {
+        userId: userId,
+        colorPick: colorPick,
+      })
+      .then((colorPick) => {
+        console.log("Added color: " + colorPick);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    //this.props.apiRequest();
+  };
   return (
     <div>
       <input
@@ -13,6 +34,7 @@ export default function ColorPicker(props) {
         onChange={(e) => console.log(e.target.value)}
         defaultValue={colorPick}
       ></input>
+      <button onClick={submit}>submit to database</button>
     </div>
   );
 }
