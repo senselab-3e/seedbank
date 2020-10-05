f5652b02b16f8140477c06ee577bbf75751997c7
cepholopoddreamz <leslie.plumb@gmail.com>
2020-10-05 03:25:47 +0000

    introducing some css features to Entryway compnoent

--- client/src/App.js
+++ client/src/App.js
@@ -21,17 +21,22 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about3e" component={About} />
-          <Route path="/oOoOs" component={Glitch} />
-          <Route path="/patches" render={() => <div>Patches</div>} />
-          <Route path="/entryway" component={Entryway} />
-          <Route exact path="/auth" component={AuthPage} />
-          <Route exact path="/events" component={withAuth(EventsPage)} />
-          <Route exact path="/traces" component={withAuth(ImagesPage)} />
-        </Switch>
+        <div
+          class="
+        root-container"
+        >
+          <Header />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about3e" component={About} />
+            <Route path="/oOoOs" component={Glitch} />
+            <Route path="/patches" render={() => <div>Patches</div>} />
+            <Route path="/entryway" component={Entryway} />
+            <Route exact path="/auth" component={AuthPage} />
+            <Route exact path="/events" component={withAuth(EventsPage)} />
+            <Route exact path="/traces" component={withAuth(ImagesPage)} />
+          </Switch>
+        </div>
       </Router>
     );
   }
--- client/src/pages/Entryway.js
+++ client/src/pages/Entryway.js
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "../style/entryway.css";
 import { createPositions } from "../helpers/Calculators";
 import styled from "styled-components";
 
 export default function Entryway() {
-  return <div></div>;
+  // eslint-disable-next-line
+  const [bcolor, setColor] = useState("#f812c0");
+  const [sliderColor, setSliderColor] = useState(
+    '{h: 317, s: "94%", l: "52%"}'
+  );
+  const [amt, setAmount] = useState(0);
+  return <div className="containerPalette"> adfds</div>;
 }
