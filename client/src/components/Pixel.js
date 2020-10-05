5b485953f28ded95daff754b5ed8c45bffcaf20d
cepholopoddreamz <leslie.plumb@gmail.com>
2020-10-05 22:54:39 +0000

    added one way of adding new pixel-patch elements to the entryway view page but i think i will change it from the straight javascript way to an object class in a discrete component

--- client/src/components/Pixel.js
+++ client/src/components/Pixel.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+// extracts function that will generate x and y positions, based on propsed passed to it. currently calculates for the whole of the browser window
 import { createPositions } from "../helpers/Calculators";
 
 const PixelPatch = styled.div.attrs((props) => ({
@@ -23,25 +24,42 @@ const PixelPatch = styled.div.attrs((props) => ({
   }
 `;
 
+const createEl = (e) => {
+  e.preventDefault();
+  var pixel = document.createElement("div");
+  pixel.className = "pixel";
+  pixel.classList.add("picnicPatch");
+  pixel.style.left = createPositions("left");
+  pixel.style.top = createPositions("top");
+  // pixel.style.backgroundColor = bcolor;
+  pixel.onclick = createEl;
+  const container = document.querySelector(".containerPalette");
+  container.appendChild(pixel);
+  console.log(pixel);
+};
+
 export default function Pixel(prop) {
   const [pColor, setColor] = useState(prop.bgHex);
   const [xPos, setXpos] = useState(createPositions("top"));
   const [yPos, setYpos] = useState(createPositions("left"));
 
+  //bring this back once the background color is scrolling hues again.
   //   useEffect(() => {
   //     setColor(prop.bgHex);
   //   }, [prop.bgHex]);
 
   //... so. a lot of traditional patterns for setting of the onClick={(){thing}}... but since i was passing down a function as a prop, by calling it within another function it was an anonymous function being triggered rather then the prop function. i erroneously had onClick={() => prop.func}}
   return (
-    <div>
+    <div class="containerPalette">
       <PixelPatch
         // left={prop.left}
         // top={prop.top}
         top={xPos}
         left={yPos}
         background={pColor}
-        onClick={prop.func}
+        onClick={(e) => {
+          createEl(e);
+        }}
       ></PixelPatch>
     </div>
   );
