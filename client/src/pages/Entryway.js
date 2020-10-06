f36a668da6edf6f6e9b92129d4818e05927bcac3
cepholopoddreamz <leslie.plumb@gmail.com>
2020-10-06 06:03:55 +0000

    found a css-based way of continuously shifting the background color without needed to an interval javascript based way of updating the color value. however, it appears that the hue-rotate means that if i use the color picker for a new color - that is passed as a prop to the entryway component - the css hue rotate wont refresh to match that color. it will take that color plus the current hue rotation number its currently moving through, since a css animation is being used infintitely to applue the hue rotation. its tricky. using pure css to apply the color shifts is ideal - but... ideally the color patch selection would be matched perfectly by the background color. but that may mean using javascript again and so ill just debate this further

--- client/src/pages/Entryway.js
+++ client/src/pages/Entryway.js
@@ -5,6 +5,20 @@ import Pixel from "../components/entryway/Pixel";
 import styled from "styled-components";
 
 const ContainerPalette = styled.div`
+  @keyframes hue-rotate {
+    from {
+      -webkit-filter: hue-rotate(0);
+      -moz-filter: hue-rotate(0);
+      -ms-filter: hue-rotate(0);
+      filter: hue-rotate(0);
+    }
+    to {
+      -webkit-filter: hue-rotate(360deg);
+      -moz-filter: hue-rotate(360deg);
+      -ms-filter: hue-rotate(360deg);
+      filter: hue-rotate(360deg);
+    }
+  }
   padding-top: 8em;
   margin: 0em;
   z-index: -1;
@@ -13,6 +27,7 @@ const ContainerPalette = styled.div`
   flex-wrap: nowrap;
   justify-content: flex-start;
   background-color: ${(props) => props.bgHex};
+  animation: hue-rotate 45s linear infinite;
   height: 100vh;
   width: 100vw;
 `;
@@ -24,13 +39,24 @@ export default function Entryway(props) {
   //     '{h: 317, s: "94%", l: "52%"}'
   //   );
   //const [amt, setAmount] = useState(0);
+  const [seconds, setSeconds] = useState(1);
 
   useEffect(() => {
     setColor(props.patchColor);
   }, [props.patchColor]);
 
+  //NOTE: this is only hear to remember the logic if i want to set up some kind of timer-based call. but a css way of shifting the colors was found, so this is no longer needed for it's original purpose
+  //   useEffect(() => {
+  //     const timer = setInterval(() => {
+  //       setSeconds(seconds + 1);
+  //     }, 1000);
+  //     // clearing interval
+  //     return () => clearInterval(timer);
+  //   });
+
   return (
     <ContainerPalette bgHex={bcolor}>
+      <p>{seconds}</p>
       <Pixel bgHex={bcolor} />
     </ContainerPalette>
   );
