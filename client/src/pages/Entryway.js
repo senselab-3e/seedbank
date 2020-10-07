79467920f051c351f721abe4ea4aabb1981103c7
cepholopoddreamz <leslie.plumb@gmail.com>
2020-10-07 02:05:15 +0000

    updated css in entryway component so that a flexbox is used allowing for multiple palettes to now be loaded relative to a prop condition check and width prop - again working toward re-usability.

--- client/src/pages/Entryway.js
+++ client/src/pages/Entryway.js
@@ -4,6 +4,17 @@ import "../style/entryway.css";
 import Pixel from "../components/entryway/Pixel";
 import styled from "styled-components";
 
+const Container = styled.div`
+  z-index: 10;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: flex-start;
+
+  padding: 0;
+  margin: 0;
+`;
+
 const ContainerPalette = styled.div`
   --animationmode: hue-rotate;
   --color-mode: ${(props) => props.colorMode};
@@ -35,14 +46,10 @@ const ContainerPalette = styled.div`
   padding-top: 8em;
   margin: 0em;
   z-index: -1;
-  display: flex;
-  flex-direction: row;
-  flex-wrap: nowrap;
-  justify-content: flex-start;
   background-color: ${(props) => props.bgHex};
   animation: var(--color-mode) 45s linear infinite;
   height: 100vh;
-  width: 100vw;
+  width: ${(props) => props.width};
 `;
 
 export default function Entryway(props) {
@@ -58,10 +65,42 @@ export default function Entryway(props) {
     console.log(`current mode has switched ${animationMode}`);
   }, [animationMode]);
 
+  //NOTE: this is only here as a reminder for how to conditionally show different components - in the future
+  function Background(props) {
+    const num = props.paletteNum;
+    if (num === "2") {
+      return (
+        <>
+          <ContainerPalette colorMode={animationMode} width="2" />
+          <ContainerPalette colorMode={animationMode} width="2" />
+        </>
+      );
+    } else if (num === "3") {
+      return (
+        <>
+          <ContainerPalette colorMode={animationMode} width="3" />
+          <ContainerPalette colorMode={animationMode} width="3" />
+          <ContainerPalette colorMode={animationMode} width="3" />
+        </>
+      );
+    } else {
+      return <ContainerPalette colorMode={animationMode} />;
+    }
+  }
+
   return (
-    <>
-      <ContainerPalette bgHex={mainBG} colorMode={animationMode} />
+    <Container>
+      <ContainerPalette
+        bgHex={mainBG}
+        colorMode={animationMode}
+        width={"50vh"}
+      />
+      <ContainerPalette
+        bgHex={mainBG}
+        colorMode={"hue-rotate"}
+        width={"50vh"}
+      />
       <Pixel bgHex={mainBG} setMode={setMode} />
-    </>
+    </Container>
   );
 }
