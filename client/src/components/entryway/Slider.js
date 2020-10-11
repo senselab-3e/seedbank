5ebd8c1ab8ee5d724738d1ad3a8affa31ba40415
cepholopoddreamz <leslie.plumb@gmail.com>
2020-10-11 22:38:22 +0000

    found an unsaved version of slider component. just pushing this in case the last commit didnt contain the working version intended in the last comit

--- client/src/components/entryway/Slider.js
+++ client/src/components/entryway/Slider.js
@@ -2,15 +2,6 @@ import React, { useState, useEffect } from "react";
 //import axios from "axios";
 import Slide from "./Slide";
 
-export const randomColors = () => {
-  var letters = "0123456789ABCDEF";
-  var color = "#";
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-};
-
 export default function Slider(props) {
   console.log("SLIDER CHILD LEVEL db res:", props.dataList);
   // eslint-disable-next-line
@@ -28,7 +19,7 @@ export default function Slider(props) {
   for (const key in dataList) {
     // console.log(dataList[key].body, "ehll");
     sliders.push(
-      <Slide key={key} color={randomColors()} text={dataList[key].body}>
+      <Slide key={key} text={dataList[key].body}>
         ddd
       </Slide>
     );
