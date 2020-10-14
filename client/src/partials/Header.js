3787d49f70bc970f14e420c886711b23eb3e0a90
cepholopoddreamz <leslie.plumb@gmail.com>
2020-10-14 21:37:11 +0000

    working toward collapsable hamburger icon menu system adding conditional functions for displaying or hiding all the menu items

--- client/src/partials/Header.js
+++ client/src/partials/Header.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-// import { FaAlignRight } from "react-icons/fa";
+import { FaAlignRight } from "react-icons/fa";
 import { SiNextdoor } from "react-icons/si";
 import { RiDoorOpenFill } from "react-icons/ri";
 import { RiLoginBoxFill } from "react-icons/ri";
@@ -14,11 +14,11 @@ import ColorPicker from "../components/entryway/ColorPicker";
 
 export default function Header(props) {
   // eslint-disable-next-line
-  const [toggle, setToggle] = useState(false);
+  const [toggle, setToggle] = useState(true);
 
-  // const Toggle = () => {
-  //   setToggle(!toggle);
-  // };
+  const Toggle = () => {
+    setToggle(!toggle);
+  };
 
   //this is useful for appearing disappearing accordian
   //<ul className={toggle ? "nav-links show-nav" : "nav-links"}></ul>
@@ -33,51 +33,58 @@ export default function Header(props) {
           colorCapture={props.colorCapture}
           patchColor={props.patchColor}
         />
-        <li>
+        <li className={toggle ? "show-nav" : "hide-nav"}>
           <Link to="/entryway">
             <RiDoorOpenFill />
             Entryway
           </Link>
         </li>
 
-        <li>
+        <li className={toggle ? "show-nav" : "hide-nav"}>
           <Link to="/auth">
             <RiLoginBoxFill />
             Login
           </Link>
         </li>
-        <li>
+        <li className={toggle ? "show-nav" : "hide-nav"}>
           <Link to="/oOoOs">
             <BiCast />
             404
           </Link>
         </li>
-        <li className="user">
+        <li className={toggle ? "show-nav user" : "hide-nav user"}>
           <Link to="/traces">
             <MdAddToPhotos />
           </Link>
         </li>
-        <li className="user">
+        <li className={toggle ? "show-nav user" : "hide-nav user"}>
           <Link to="/events">
             <BiChair />
           </Link>
         </li>
 
-        <li className="user">
+        <li className={toggle ? "show-nav user" : "hide-nav user"}>
           <Link to="/">
             <SiNextdoor />
           </Link>
         </li>
 
-        <li className="user">
+        <li className={toggle ? "show-nav user" : "hide-nav user"}>
           <Link to="/about3e">
             <RiQuestionLine />
           </Link>
+        </li>
+        <li>
+          <button onClick={Toggle}>
+            <FaAlignRight />
+          </button>
         </li>
       </ul>
     </nav>
   );
 }
+
+// className="miniMenu"
 
 // <div className="navBar">
 //   <button onClick={Toggle}>
