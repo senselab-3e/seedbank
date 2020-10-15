1bce809c9509f32516167c8520f9c54869f25da3
cepholopoddreamz <leslie.plumb@gmail.com>
2020-10-15 03:04:14 +0000

    sucessfully added a debounce library and useCallback hook to debounce the colorcapture from the colorpicker component

--- client/src/components/entryway/ColorPicker.js
+++ client/src/components/entryway/ColorPicker.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useCallback } from "react";
 import debounce from "lodash.debounce";
 // import { UserId } from "../GetUser";
 // import axios from "axios";
@@ -7,36 +7,19 @@ import debounce from "lodash.debounce";
 
 export default function ColorPicker(props) {
   const [colorPick, setColorPick] = useState(props.patchColor || "#000000");
-  const [debouncedVal, saveToDb] = useState(""); // would be an API call normally
+  //const [debouncedVal, saveToDb] = useState(""); // would be an API call normally /// but my props.colorCapture takes the value and passes up to a parent app and then back down to other components that handle the saving.
 
-  const sendColor = (e) => {
-    if (props.colorCapture) {
-      console.log(e.target.value, " assignment");
-      props.colorCapture(e.target.value);
-    }
+  const debouncedSave = useCallback(
+    debounce((nextVal) => props.colorCapture(nextVal), 100),
+    [] // will be created only once initially
+  );
+
+  const onChange = (e) => {
+    const { value: nextValue } = e.target;
+    setColorPick(nextValue);
+    debouncedSave(nextValue);
   };
 
-  useEffect(() => {
-    setColorPick(props.patchColor);
-  }, [props.patchColor]); //the setColorPick will run only when this prop value changes
-
-  //Previously i had the color input and the button for saving that color, all in this component. but i then needed to separate out the button. just keeping this code here for now, in case i change my mind.
-  // const submitColorVal = (e) => {
-  //   const userId = UserId();
-  //   console.log(colorPick, userId, "clicked");
-  //   e.preventDefault();
-  //   axios
-  //     .put(`/api/auth/${userId}`, {
-  //       userId: userId,
-  //       colorPick: colorPick,
-  //     })
-  //     .then((colorPick) => {
-  //       console.log("Added color: " + colorPick);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // };
   return (
     <>
       <li>
@@ -44,7 +27,7 @@ export default function ColorPicker(props) {
           type="color"
           className="menuColorPicker"
           onClick={(e) => setColorPick(e.target.value)}
-          onChange={(e) => sendColor(e)}
+          onChange={(e) => onChange(e)}
           defaultValue={colorPick}
         ></input>
         Select Color
@@ -56,3 +39,21 @@ export default function ColorPicker(props) {
 /* <button onClick={submitColorVal}>Save Color</button> */
 
 //I'm going to potentially move this button to another level, or just when the entryway view is activated... which means setting up a callback and props system to pass the color value up to the app level....
+
+//Previously i had the color input and the button for saving that color, all in this component. but i then needed to separate out the button. just keeping this code here for now, in case i change my mind.
+// const submitColorVal = (e) => {
+//   const userId = UserId();
+//   console.log(colorPick, userId, "clicked");
+//   e.preventDefault();
+//   axios
+//     .put(`/api/auth/${userId}`, {
+//       userId: userId,
+//       colorPick: colorPick,
+//     })
+//     .then((colorPick) => {
+//       console.log("Added color: " + colorPick);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// };
