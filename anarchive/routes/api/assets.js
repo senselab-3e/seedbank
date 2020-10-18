bc6e01f03bb69dcdda54b699e7bb45df4b1fec3c
cepholopoddreamz <leslie.plumb@gmail.com>
2020-10-18 00:49:53 +0000

    risky part 2

--- anarchive/routes/api/assets.js
+++ anarchive/routes/api/assets.js
@@ -1,57 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const knex = require("../../db/knex.js");
-const assets = require("../../services/asset_path_helpers");
-const multer = require("multer");
+const assets = require("../../services/asset_helpers");
 const path = require("path");
 const fs = require("fs");
-
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    // although it won't be tracked by git, you'll probably want to delete
-    // anything uploaded here after you're done developing
-    // if (process.env.NODE_ENV != "production") {
-    //   return cb(null, "./assets");
-    // }
-    var dir = assets.hashPath(file, req.url.split("/")[1]);
-    fs.access(dir, (notfound) => {
-      if (notfound) {
-        return fs.mkdir(dir, { recursive: true }, (err) => {
-          return cb(err, dir);
-        });
-      } else {
-        return cb(null, dir);
-      }
-    });
-  },
-  filename: function (req, file, cb) {
-    // if (process.env.NODE_ENV != "production") {
-    //   var dir = "./assets";
-    // } else {
-    var dir = assets.hashPath(file, req.url.split("/")[1]);
-    // }
-
-    var file_loc = path.join(dir, file.originalname);
-    fs.access(file_loc, (notfound) => {
-      if (notfound) {
-        return cb(null, file.originalname);
-      } else {
-        fs.readdir(dir, (err, files) => {
-          var arr = file.originalname.split(".");
-          var newName =
-            arr.slice(0, -1).join(".") +
-            "_" +
-            files.length +
-            "." +
-            arr.slice(-1).toString();
-          return cb(err, newName);
-        });
-      }
-    });
-  },
-});
-
-var upload = multer({ storage: storage });
 
 // GET api/assets/images
 router.get("/images/:id", (req, res) => {
@@ -59,7 +11,7 @@ router.get("/images/:id", (req, res) => {
     .where({ id: req.params.id })
     .first()
     .then((image) => {
-      res.sendFile(assets.getPath(image, req.url.split("/")[1]));
+      res.sendFile(assets.get_path(image, assets.get_type(req.url)));
     })
     .catch((err) => {
       console.log(err);
@@ -72,7 +24,7 @@ router.get("/gifverse/:id", (req, res) => {
     .where({ id: req.params.id })
     .first()
     .then((gif) => {
-      res.sendFile(assets.getPath(gif, req.url.split("/")[1]));
+      res.sendFile(assets.get_path(gif, assets.get_type(req.url)));
     })
     .catch((err) => {
       console.log(err);
@@ -80,33 +32,22 @@ router.get("/gifverse/:id", (req, res) => {
 });
 
 // POST api/assets/images
-router.post("/images", upload.single("image"), (req, res) => {
-  // if (process.env.NODE_ENV != 'production') { res.send(console.log(req.file)) }
-  // else {
-  knex("images")
-    .insert({
-      name: req.file.filename,
-      type: req.body.type,
-      tendencies: req.body.tendencies,
-      notes: req.body.notes,
-      // filetype: req.file.originalname
-      //   .split(".")
-      //   .slice(-1)
-      //   .toString()
-      //   .toLowerCase(),
-      path: req.file.destination.replace("assets/images/", ""),
-    })
-    .then((id) => {
-      res.send("Successfully uploaded image");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  // }
-});
+router.post(
+  "/images",
+  assets.upload_form().single("image"),
+  async (req, res) => {
+    if (process.env.NODE_ENV != "production") {
+      res.send(console.log(req.file));
+    }
+    // else { assets.db_insert(req.file, req.body, (result) => { res.send(result) }); }
+    else {
+      res.send(await assets.db_insert(req.file, req.body));
+    }
+  }
+);
 
 // POST api/assets/gifverse
-router.post("/gifverse", upload.single("image"), (req, res) => {
+router.post("/gifverse", assets.upload_form().single("image"), (req, res) => {
   if (process.env.NODE_ENV != "production") {
     res.send(console.log(req.file));
   } else {
--- anarchive/services/asset_path_helpers.js
+++ anarchive/services/asset_path_helpers.js
@@ -1,20 +1,185 @@
-const path = require('path');
-const md5 = require('md5');
-const fs = require('fs');
+const path = require("path");
+const md5 = require("md5");
+const fs = require("fs");
+const https = require("https");
+const multer = require("multer");
+const knex = require("../db/knex.js");
+const default_loc = "./assets";
 
-function getPath(asset, type) {
-	return path.resolve(process.cwd(), 'assets', type, asset['path'], asset['name']);
-};
+function get_type(url) {
+  return url.split("/")[1];
+}
 
-function hashPath(asset, type) {
+function get_path(asset, type = "images") {
+  return path.resolve(
+    process.env.DB_HOST,
+    "assets",
+    type,
+    asset["path"],
+    asset["name"]
+  );
+}
 
-	var hash = md5(asset.originalname);
-	var chunked = hash.match(/.{1,2}/g).slice(0,4).join('/');
+function hash_path(filename, type = "images") {
+  var hash = md5(filename);
+  var chunked = hash
+    .match(/.{1,2}/g)
+    .slice(0, 4)
+    .join("/");
+  return path.join("./assets", type, chunked);
+}
 
-    return path.join('./assets', type, chunked);
-};
+function set_dest(filename, type = "images") {
+  return new Promise((resolve, reject) => {
+    var dir =
+      process.env.NODE_ENV == "production"
+        ? hash_path(filename, type)
+        : default_loc;
+    fs.access(dir, (notfound) => {
+      if (notfound) {
+        fs.mkdir(
+          dir,
+          {
+            recursive: true,
+          },
+          (err) => {
+            reject(err);
+          }
+        );
+      }
+      resolve(dir);
+    });
+  });
+}
+
+function set_filename(originalname, type = "images") {
+  return new Promise((resolve, reject) => {
+    var dir =
+      process.env.NODE_ENV == "production"
+        ? hash_path(originalname, type)
+        : default_loc;
+    var file_loc = path.join(dir, originalname);
+    fs.access(file_loc, (notfound) => {
+      if (notfound) {
+        resolve(originalname);
+      } else {
+        fs.readdir(
+          dir,
+          {
+            withFileTypes: true,
+          },
+          (err, files) => {
+            if (err) {
+              reject(err);
+            }
+            var arr = originalname.split(".");
+            var finalname =
+              arr.slice(0, -1).join(".") +
+              "_" +
+              files.filter((f) => f.isFile()).length +
+              "." +
+              arr.slice(-1).toString();
+            resolve(finalname);
+          }
+        );
+      }
+    });
+  });
+}
+
+function upload_form() {
+  var storage = multer.diskStorage({
+    destination: async function (req, file, cb) {
+      try {
+        var result = await set_dest(file.originalname, get_type(req.url));
+        cb(null, result);
+      } catch (err) {
+        cb(err, null);
+      }
+    },
+    filename: async function (req, file, cb) {
+      try {
+        var result = await set_filename(file.originalname, get_type(req.url));
+        cb(null, result);
+      } catch (err) {
+        cb(err, null);
+      }
+    },
+  });
+  return multer({
+    storage: storage,
+  });
+}
+
+function upload_from_url(url, auth_token) {
+  return new Promise(async (resolve, reject) => {
+    var originalname = url.split("/").pop();
+    var dest = await set_dest(originalname);
+    var filename = await set_filename(originalname);
+    var full_dest = path.join(dest, filename);
+    var file_loc = fs.createWriteStream(full_dest);
+    var options = {
+      headers: {
+        Authorization: "Bearer " + auth_token,
+      },
+    };
+    var data = {
+      filename: filename,
+      destination: dest,
+    };
+
+    https
+      .get(url, options, (response) => {
+        var stream = response.pipe(file_loc);
+        stream.on("finish", () => {
+          var file_data = {
+            destination: dest,
+            filename: filename,
+          };
+          return resolve(file_data);
+        });
+      })
+      .on("error", (err) => {
+        fs.unlink(full_dest);
+        return reject(err.message);
+      });
+  });
+}
+
+function db_insert(file, meta, return_id = false) {
+  return new Promise((resolve, reject) => {
+    var table = file.destination.split("assets/").pop().split("/")[0];
+    var row = {
+      name: file.filename,
+      path: file.destination.split(/assets\/.+?\//).pop(),
+    };
+
+    if (typeof meta.type !== "undefined") {
+      row.type = meta.type;
+    }
+    if (typeof meta.tendencies !== "undefined") {
+      row.tendencies = meta.tendencies;
+    }
+    if (typeof meta.notes !== "undefined") {
+      row.notes = meta.notes;
+    }
+
+    knex(table)
+      .insert(row)
+      .then((id) => {
+        resolve(return_id ? id[0].toString() : "uploaded asset!");
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+}
 
 module.exports = {
-    getPath,
-    hashPath
-}
+  get_type,
+  get_path,
+  hash_path,
+  upload_form,
+  upload_from_url,
+  db_insert,
+};
