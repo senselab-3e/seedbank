169a8e84ba53783524065b00c8105259f37e01b5
cepholopoddreamz <leslie.plumb@gmail.com>
2020-10-20 22:24:28 +0000

    very awkward way of handling things but at least im getting a specific image reply

--- client/src/components/ImageRecent.js
+++ client/src/components/ImageRecent.js
@@ -3,20 +3,41 @@ import axios from "axios";
 
 export default function ImageRecent() {
   const [lastImage, setLastImage] = React.useState("");
+  const [id, setId] = React.useState(null);
+
+  const getRecent = () => {
+    axios
+      .get(`/api/assets/images/lookup/${id}`)
+      .then((image) => {
+        console.log(image.data[0]);
+        //setLastImage(image);
+      })
+      .catch((err) => console.log(err));
+  };
 
   const getImageEntry = () => {
     axios
       .get("/api/assets/images/latest")
       .then((images) => {
         //setLastImage(images.data);
-        console.log(images.data);
+        console.log(images.data["max(`id`)"]);
+        setId(images.data["max(`id`)"]);
+        // getRecent(images.data["max(`id`)"]);
       })
       .catch((err) => console.log(err));
   };
 
-  React.useEffect(() => {
+  useEffect(() => {
     getImageEntry();
-  }, [getImageEntry]);
+  }, []);
 
-  return <div>{lastImage}</div>;
+  console.log(id, lastImage);
+
+  return (
+    <div>
+      {lastImage}
+      <button onClick={getImageEntry}>first</button>
+      <button onClick={getRecent}>second</button>
+    </div>
+  );
 }
