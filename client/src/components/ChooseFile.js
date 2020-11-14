6cc282dafdf369ca6dd95e4e934570abe5b2db15
cepholopoddreamz <leslie.plumb@gmail.com>
2020-11-14 06:31:41 +0000

    adding some css to button elements

--- client/src/components/ChooseFile.js
+++ client/src/components/ChooseFile.js
@@ -1,11 +1,17 @@
 import React from "react";
 
 export default function ChooseFile(props) {
-  return (
-    <form encType="multipart/form-data">
-      <label>
-        <input type="file" name="image" onChange={props.onChange} />
-      </label>
-    </form>
-  );
+  if (props.onChange) {
+    return (
+      <form encType="multipart/form-data">
+        <label>
+          <input type="file" name="image" onChange={props.onChange} />
+        </label>
+      </form>
+    );
+  } else {
+    return (
+      <div>an onChange function missing in props for form submit element"</div>
+    );
+  }
 }
