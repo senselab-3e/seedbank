b324baf2b690786c91c845021c5be3130e94d8a6
cepholopoddreamz <leslie.plumb@gmail.com>
2020-11-15 05:53:14 +0000

    css adjustments for mobile view, on input tags

--- client/src/components/ChooseFile.js
+++ client/src/components/ChooseFile.js
@@ -4,9 +4,11 @@ export default function ChooseFile(props) {
   if (props.onChange) {
     return (
       <form encType="multipart/form-data">
-        <label>
-          <input type="file" name="image" onChange={props.onChange} />
-        </label>
+        <div className="button-wrap">
+          <label className="new-button">
+            <input type="file" name="image" onChange={props.onChange} />
+          </label>
+        </div>
       </form>
     );
   } else {
