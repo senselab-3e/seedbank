710905c59f823da017712d00c4fe9d21cfcee466
Joel Auerbach <joelauerbach1@gmail.com>
2021-01-24 01:01:31 -0800

    cleanup

--- anarchive/routes/api/slack.js
+++ anarchive/routes/api/slack.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const knex = require('../../db/knex.js');
-const axios = require('axios');
 
 const slack = require('../../services/slack_helpers');
 
--- anarchive/routes/api/texts.js
+++ anarchive/routes/api/texts.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../../db/knex.js');
 
-// GET api/texts/:source
+// GET api/texts
 router.get('/', (req, res) => {
 	knex('texts')
-		.where( req.params.source ? { source: req.params.source } : {})
+		.where( req.query.source ? { source: req.query.source } : {})
 		.then(texts => { res.send(texts); })
 		.catch(err => { console.log(err); })
 });
